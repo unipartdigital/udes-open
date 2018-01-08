9f33f75477bbbe3327f4bf4bb071974bc707e35a
WhiteShadoW <miquel.palahi.sitges@unipart.io>
2018-01-08 10:28:35 +0000

    Tidy up stock.picking.create_transfer: - Add docstring of the function - Add result_package_id handling - Removed several comments

--- addons/udes_core/models/stock_picking.py
+++ addons/udes_core/models/stock_picking.py
@@ -12,15 +12,29 @@ class StockPicking(models.Model):
             self,
             quant_ids,
             location_id,
+            picking_type_id=None,
             location_dest_id=None,
             result_package_id=None,
-            result_package_name=None,
             move_parent_package=False,
-            picking_type_id=False,
             **kwargs
     ):
-        """
-            TODO
+        """ Creates a stock.picking and stock.moves for a given list
+            of stock.quant ids
+
+            @param quant_ids: Array (int)
+                An array of the quants ID to add to the stock.picking
+            @param location_id: int
+                ID of the location where the stock.picking is moving from.
+            @param (optional) picking_type_id: int
+                The type of the stock.picking.
+            @param (optional) location_dest_id: int
+                ID of the location where the stock is going to be moved to.
+            @param (optional) result_package_id: int
+                The target package ID
+            @param (optional) move_parent_package: Boolean
+                Used in pallets/nested packages, to maintain the move of the entire pallet.
+                Defaults to False
+
         """
         Picking = self.env['stock.picking']
         PickingType = self.env['stock.picking.type']
@@ -52,7 +66,7 @@ class StockPicking(models.Model):
         quants.ensure_not_reserved()
         quants.ensure_entire_packages()
 
-        # transfer needs to be created
+        # Create stock.picking
         vals = {
             'picking_type_id': picking_type.id,
             'location_dest_id': location_dest_id,
@@ -62,10 +76,9 @@ class StockPicking(models.Model):
         picking = Picking.create(vals)
 
         # create stock moves
-        # TODO: create function to create moves from quants, action_assign and action_confirm?
+        # TODO: create function to create moves from quants + call action_assign and action_confirm?
         # TODO: create a function at quants that returns the group ?
         # TODO: get all the display_names with one call to DB?
-        #created_moves = Move.browse()
         for product_id, qty in group_qty_by_product(quants).items():
             move_vals = {
                     'name': '{} {}'.format(qty, Product.browse(product_id).display_name),
@@ -77,17 +90,9 @@ class StockPicking(models.Model):
                     'picking_id': picking.id,
                 }
             move = Move.create(move_vals)
-            #created_moves |= move
 
-        # call action_confirm:
-        # - chain of moves will be created if needed
-        # - procurement group will be created if needed (this needs u_create_group)
-        # TODO: migrate picking_type.u_create_group and extend action_confirm
-        #       probably not needed for stock move
-        # TODO: picking.action_confirm or created_moves._action_confirm ?
+        # Use picking.action_cofirm or moves._action_confirm()
         picking.action_confirm()
-        # action confirm merges moves of the same product if merge=True
-        #created_moves._action_confirm(merge=False) #TODO: picking or create_moves?
 
         # Use picking.action_assign or moves._action_assign to create move lines
         # Context variables:
@@ -95,26 +100,15 @@ class StockPicking(models.Model):
         #   filters the quants that stock.quant._gather returns
         # - bypass_reservation_update:
         #   avoids to execute code specific for Odoo UI at stock.move.line.write()
-        # TODO: picking.action_assign or created_moves._action_assign ?
         picking.with_context(quant_ids=quant_ids, bypass_reservation_update=True).action_assign()
-        #created_moves.with_context(quant_ids=quant_ids, bypass_reservation_update=True)._action_assign()
 
-        """
-        # TODO: result_package
-        # TODO: track_parent_package
-        #       if it is false remove parent_id of the result_package_id ??
-        # 1) create one operation per package_id in quants (corresponding to quant_ids)
-           # if the package is in a parent_package e.g. pallet (pack_id.parent_id is set)
-            # we have to set the result_package_id, otherwise we will lose
-            # the parent package (e.g. pallet) info.
-            if result_package_id:
-                op_vals['result_package_id'] = result_package_id
-            elif track_parent_package and pack.parent_id:
-                op_vals['result_package_id'] = pack.parent_id.id
-        # 2) create one operation for each product without packages
-            # case where we are moving quants from A to B and they have to be in a package in location B
-            if result_package_id:
-                op_vals['result_package_id'] = result_package_id
-        """
+        if result_package_id:
+            picking.move_line_ids.write({'result_package_id': result_package_id})
+        # TODO: this might be in the package_hierarchy module, because odoo by default
+        #       does not handle parent packages
+        if not move_parent_package:
+            # when false remove parent_id of the result_package_id ??
+            #picking.move_line_ids.mapped('result_package_id').write({'package_id': False})
+            pass
 
         return picking
