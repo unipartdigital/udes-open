75f866cd94a2d1aac5929b17f947a7356b482705
WhiteShadoW <miquel.palahi.sitges@unipart.io>
2018-01-09 12:03:05 +0000

    Updated create_picking and add update_picking methods - Tidy up stock.picking.create_picking() - Add stock.picking.ensure_valid_state() - Add stock.picking.create_move_from_quants() - Add stock.picking.update_picking() - Fix stock.quant.ensure_not_rerved() - Add stock.quant.ensure_valid_location() - Remove group_qty_by_product from common.py - Add stock.quant.group_quantity_by_product - Add stock.quant.total_quantity()

--- addons/udes_core/models/stock_quant.py
+++ addons/udes_core/models/stock_quant.py
@@ -1,4 +1,6 @@
-from odoo import models, fields, _
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields, _
 from odoo.exceptions import ValidationError
 
 
@@ -11,7 +13,7 @@ class StockQuant(models.Model):
         if reserved:
             raise ValidationError(_('Items are reserved and cannot be moved. '
                                     'Please speak to a team leader to resolve '
-                                    'the issue.\nAffected Items:') % (
+                                    'the issue.\nAffected Items: %s') % (
                                         ' '.join(reserved.mapped('package_id.name'))
                                         if reserved.mapped('package_id')
                                         else ' '.join(reserved.mapped('product_id.display_name'))))
@@ -29,6 +31,21 @@ class StockQuant(models.Model):
                                     'Incomplete Packages:\n'
                                     '%s') % (' '.join(prob_packs.mapped('name'))))
 
+    def ensure_valid_location(self, location_id):
+        """ Ensure the recorset self contains quants child of location_id
+        """
+        # TODO: check this function again, create generic is_valid/are_valid?
+        Location = self.env['stock.location']
+        n_quant_locs = len(self.mapped('location_id'))
+        child_locs = Location.search([
+                ('id', 'child_of', location_id),
+                ('id', 'in', self.mapped('location_id.id'))
+            ])
+        if len(child_locs) != n_quant_locs:
+            raise ValidationError(
+                    _('The locations of some quants are not children of'
+                      ' location %s') %
+                        Location.browse(location_id).name)
 
     def _gather(self, product_id, location_id, **kwargs):
         """ Call default _gather function, if quant_ids context variable
@@ -41,3 +58,20 @@ class StockQuant(models.Model):
         if quant_ids:
             quants = quants.filtered(lambda q: q.id in quant_ids)
         return quants
+
+    @api.multi
+    def total_quantity(self):
+        """ Returns the total quantity of the quants in self
+        """
+        return sum(self.mapped('quantity'))
+
+    @api.multi
+    def group_quantity_by_product(self):
+        """ Returns a dictionary with the total quantity per product,
+            mapped by product_id.
+        """
+        products = {}
+        for quant in self:
+            products.setdefault(quant.product_id.id, 0)
+            products[quant.product_id.id] += quant.quantity
+        return products
