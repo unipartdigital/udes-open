5e622792301ec89a1aaa5cc627ed6e85ea433005
Miquel PS <miquel.palahi.sitges@unipart.io>
2018-01-31 15:31:17 +0000

    Tidy up and rename _prepare_packages() to _prepare_result_packages()

--- addons/udes_api/models/stock_move_line.py
+++ addons/udes_api/models/stock_move_line.py
@@ -12,65 +12,74 @@ class StockMoveLine(models.Model):
         """ Extend mark_as_done function to handle another level of packages and
             use u_target_storage_format.
         """
-
         result_package, parent_package = \
-                self._prepare_packages(package, result_package, products_info)
+                self._prepare_result_packages(package, result_package, products_info)
 
         res = super(StockMoveLine, self).mark_as_done(location_dest=location_dest,
-                                                  result_package=result_package,
-                                                  package=package,
-                                                  products_info=products_info)
+                                                      result_package=result_package,
+                                                      package=package,
+                                                      products_info=products_info)
 
         if parent_package:
             res.write({'u_result_parent_package_id': parent_package.id})
 
         return res
 
-    def _prepare_packages(self, package, result_package, products_info):
-        # TODO: outer if should be target storage format since it is
-        #       what defines the logic of the parameters
-
+    def _prepare_result_packages(self, package, result_package, products_info):
+        """ Compute result_package and result_parent based on the
+            u_target_Storage_format of the picking type + the input
+            parameters.
+        """
         Package = self.env['stock.quant.package']
+
+        parent_package = None
 
         # atm mark_as_done is only called per picking
         picking = self.mapped('picking_id')
         picking.ensure_one()
-
         target_storage_format = picking.picking_type_id.u_target_storage_format
 
-        parent_package = None
-        # error when trying to mark_as_done a full package or setting result package
-        # when result storage format is products
-        if (result_package or (package and not products_info)) and \
-                target_storage_format == 'product':
-            raise ValidationError(
-                    _('Invalid parameters for target storage format.'))
-
-        if result_package:
-            if target_storage_format == 'pallet_packages':
+        if target_storage_format == 'pallet_packages':
+            if result_package:
+                # At pallet_packages, result_package parameter is expected
+                # to be the result_parent_package of the move_line
+                # It migt be a new pallet id
                 parent_package = Package.get_package(result_package, create=True)
                 result_package = None
                 if not package:
                     if products_info:
-                        # we are packing
+                        # Products are being packed
                         result_package = Package.create({}).name
                     elif not all([ml.result_package_id for ml in self]):
-                       raise ValidationError(_("Some of the move lines don't have result package."))
+                        # Setting result_parent_package expects to have
+                        # result_package for all the move lines
+                        raise ValidationError(
+                                _("Some of the move lines don't have result package."))
+            elif products_info:
+                raise ValidationError(
+                        _('Invalid parameters for target storage format,'
+                          ' expecting result package.'))
 
-            elif target_storage_format == 'pallet_products':
-                # TODO: merge with line 67 ?
-                if not package:
-                    # moving stock into a pallet of products, result_package might be new
-                    result_package = Package.get_package(result_package, create=True).name
-        elif package:
-            # mark_as_done package without result_package
-            pass
-        elif products_info:
-            # mark_as_done products without package or result_package
-            if target_storage_format == 'package':
-                # create result_package when packing products without
+        elif target_storage_format == 'pallet_products':
+            if result_package and not package:
+                # Moving stock into a pallet of products, result_package
+                # might be new pallet id
+                result_package = Package.get_package(result_package, create=True).name
+
+        elif target_storage_format == 'package':
+            if products_info and not package and not result_package:
+                # Mark_as_done products without package or result_package
+                # Create result_package when packing products without
                 # result_package being set
                 result_package = Package.create({}).name
+
+        elif target_storage_format == 'product':
+            # Error when trying to mark_as_done a full package or setting result package
+            # when result storage format is products
+            if result_package or (package and not products_info):
+                raise ValidationError(
+                        _('Invalid parameters for products target'
+                          ' storage format.'))
 
         return (result_package, parent_package)
 
