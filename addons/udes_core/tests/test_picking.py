591dfbbb1178222285b09270ec4fd234a07b70e6
nocturnalastro <michele.costa@unipart.io>
2018-02-15 11:29:29 +0000

    [WIP] tests for update_picking

--- addons/udes_core/tests/test_picking.py
+++ addons/udes_core/tests/test_picking.py
@@ -12,7 +12,6 @@ class TestGoodsInPicking(common.BaseUDES):
         User = cls.env['res.users']
         PickingType = cls.env['stock.picking.type']
         Picking = cls.env['stock.picking']
-        Package = cls.env['stock.quant.package']
 
         user_warehouse = User.get_user_warehouse()
         # Get goods in type
@@ -61,7 +60,6 @@ class TestGoodsInPicking(common.BaseUDES):
         returned_pickings = Picking.get_pickings(origin=self.test_picking.origin)
         self.assertEqual(returned_pickings.id, self.test_picking.id)
 
-
     def test05_get_info_all(self):
         """ Tests get_info without requesting
             a field
@@ -81,7 +79,6 @@ class TestGoodsInPicking(common.BaseUDES):
         # Sorted returns a list(or did when I wrote this)
         # so no need to type cast
         self.assertEqual(sorted(info[0].keys()), sorted(expected))
-
     
     def test06_get_info_only_id(self):
         """ Tests get_info requesting a specific field"""
@@ -89,83 +86,4 @@ class TestGoodsInPicking(common.BaseUDES):
         # There should only be one and they should all be the same if not
         self.assertEqual(list(info[0].keys()), ['id'])
 
-    def test07_update_picking_validate_complete(self):
-        """ Test update_picking by addding by splitting a product
-            without serial numbers
-        """
-        products_info = [{'product_barcode': self.apple.barcode, 'qty': 10}]
-        self.test_picking.update_picking(products_info=products_info)
-        self.assertEqual(self.test_picking.move_lines.quantity_done, 10)
-        
-        self.test_picking.update_picking(validate=True)
-        self.assertEqual(self.test_picking.move_lines.state, 'done')
 
-    
-    def test08_update_picking_validate_done_complete_serial_number(self):
-        """ Test update_picking by addding by splitting a product
-            without serial numbers
-        """
-        products_info = [{
-                            'product_barcode': self.apple.barcode, 
-                            'qty': 10, 
-                            'serial_numbers': ['Apple1', 'Apple2', 'Apple3', 'Apple4', 'Apple5',
-                                               'Apple6', 'Apple7', 'Apple8', 'Apple9', 'Apple10']
-                          }] 
-        self.test_picking.update_picking(products_info=products_info)
-        self.assertEqual(self.test_picking.move_lines.quantity_done, 10)
-        
-        self.test_picking.update_picking(validate=True)
-        self.assertEqual(self.test_picking.move_lines.state, 'done')
-        
-
-    def test09_update_picking_split_lanes(self):
-        # Without deep copy this will need remaking!
-        products_info = [{'product_barcode': self.apple.barcode, 'qty': 5}]
-        
-        self.test_picking.update_picking(products_info=products_info)
-        moves_lines = self.test_picking.move_line_ids
-        self.assertEqual(sorted(moves_lines.mapped('qty_done')), sorted([0.0, 5.0]))
-        
-        self.test_picking.update_picking(products_info=products_info)
-        moves_lines = self.test_picking.move_line_ids
-        self.assertEqual(sorted(moves_lines.mapped('qty_done')), sorted([5.0, 5.0]))
-        
-        self.test_picking.update_picking(validate=True)
-        self.assertEqual(self.test_picking.move_lines.state, 'done')
-
-    def test10_update_picking_split_lanes_with_serial_number(self):
-        products_info = [{
-                            'product_barcode': self.apple.barcode, 
-                            'qty': 5, 
-                            'serial_numbers': ['Apple1', 'Apple2', 'Apple3', 'Apple4', 'Apple5']
-                          }]
-        self.test_picking.update_picking(products_info=products_info)
-        moves_lines = self.test_picking.move_line_ids
-        self.assertEqual(sorted(moves_lines.mapped('qty_done')), sorted([0.0, 5.0]))
-        
-        self.test_picking.update_picking(products_info=products_info)
-        moves_lines = self.test_picking.move_line_ids
-        self.assertEqual(sorted(moves_lines.mapped('qty_done')), sorted([5.0, 5.0]))
-        
-        self.test_picking.update_picking(validate=True)
-        self.assertEqual(self.test_picking.move_lines.state, 'done')
-
-    def test11_update_picking_incomplete_validate_fail(self):
-        products_info = [{'product_barcode': self.apple.barcode, 'qty': 9}]
-        self.test_picking.update_picking(products_info=products_info)
-        with self.assertRaises(ValidationError) as e:
-            self.test_picking.update_picking(validate=True)
-        self.assertEqual(e.exception.name, 'Cannot validate transfer because there are move lines todo')
-        
-    def test12_update_picking_unequal_serial(self):
-        self.apple.tracking = "serial"
-        products_info = [{
-                            'product_barcode': self.apple.barcode,
-                            'qty': 5, 
-                            'serial_numbers': ['Apple1', 'Apple2', 'Apple3', 'Apple4']
-                          }]
-        self.test_picking.update_picking(products_info=products_info)
-        with self.assertRaises(ValidationError) as e:
-            self.test_picking.update_picking(validate=True)
-        self.assertEqual(e.exception.name, 'The number of serial numbers and quantity done does not ' \
-                                           'match for product Test product Apple')
