92acef055af3e9dd442b712de6e5e365b6ff9ca7
parisiale <parisiale@gmail.com>
2018-02-28 13:12:40 +0000

    Add stock-picking-batch POST endpoint
    Such endpoint creates a batch for the current user.

--- addons/udes_api/controllers/stock_picking_batch.py
+++ addons/udes_api/controllers/stock_picking_batch.py
@@ -27,3 +27,29 @@ class PickingBatchApi(UdesApi):
         batch = PickingBatch.get_single_batch()
 
         return batch.get_info() if batch else {}
+
+    @http.route('/api/stock-picking-batch/',
+                type='json', methods=['POST'], auth='user')
+    def create_batch_for_user(self,
+                              picking_priorities=None,
+                              max_locations=None):
+        """
+        Create a batch for the user if pickings are available.
+
+        If a batch is created, return a JSON object with the ID of
+        the batch ('id' string) and the list of pickings ('picking_ids'
+        array of stock.picking objects), otherwise return an empty
+        object.
+
+        Raises a ValidationError in case a batch already exists for
+        the current user.
+
+        @param picking_priorities - (optional) List of priorities to
+            search for the pickings
+        @param max_locations - (optional) Max number of locations to
+            pick from (not used)
+        """
+        PickingBatch = request.env['stock.picking.batch']
+        batch = PickingBatch.create_batch(picking_priorities)
+
+        return batch.get_info() if batch else {}
