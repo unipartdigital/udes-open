c20964bbfe9226d47b1ad723d0e97f3854371063
parisiale <parisiale@gmail.com>
2018-02-28 10:41:48 +0000

    First cut of StockPickingBatch

--- addons/udes_core/models/__init__.py
+++ addons/udes_core/models/__init__.py
@@ -6,6 +6,7 @@ from . import stock_location
 from . import stock_move
 from . import stock_move_line
 from . import stock_picking
+from . import stock_picking_batch
 from . import stock_picking_type
 from . import stock_quant
 from . import stock_quant_package
---
+++ addons/udes_core/models/stock_picking_batch.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class StockPickingBatch(models.Model):
+    _inherit = 'stock.picking.batch'
+
+    @api.multi
+    def get_single_batch(self, user_id=None):
+        """
+        Retrieve ...
+        Can return None ...
+        """
+        PickingBatch = self.env['stock.picking.batch']
+
+        if user_id is None:
+            user_id or self.env.user.id
+
+        batches = PickingBatch.search(
+            [('user_id', '=', user_id),
+             ('state', '=', 'in_progress')])
+        batch = None
+
+        if not batches:
+            batch = self._create_batch(user_id)
+        else:
+            if len(batches) > 1:
+                raise ValidationError(
+                    _("Expected single picking batch, found %d batches")
+                    % len(batches))
+
+            batch = batches[0]
+
+        return batch
+
+    def _get_single_batch_info(self, batch, allowed_picking_states):
+        filtered_pickings = batch.picking_ids.filtered(
+            lambda x: x.state in allowed_picking_states)
+
+        return {'id': self.id,
+                'picking_ids': filtered_pickings.get_info()}
+
+    def get_info(self, allowed_picking_states=None):
+        """
+        Return ...
+        """
+        if allowed_picking_states is None:
+            allowed_picking_states = ['assigned']
+
+        return [self._get_single_batch_info(batch, allowed_picking_states)
+                for batch in self]
+
+
+    @api.multi
+    def create_batch(self, user_id=None):
+        """
+        Creeate and return a batch for the current user if pickings
+        exist. Return None otherwise.
+        """
+        user_id = user_id or self.env.user.id
+        self._check_batches(user_id)
+
+        return self._create_batch(user_id)
+
+    def _create_batch(self, user_id):
+        Picking = self.env['stock.picking']
+        PickingBatch = self.env['stock.picking.batch']
+        Users = self.env['res.users']
+
+        warehouse = Users.get_user_warehouse()
+        picking_type_id = warehouse.pick_type_id
+        search_domain = [('picking_type_id', '=', picking_type_id.id),
+                         ('state', '=', 'assigned'),
+                         ('batch_id', '=', False)]
+        picking = Picking.search(search_domain,
+                                 order='priority desc, scheduled_date, id',
+                                 limit=1)
+
+        if not picking:
+            return None
+
+        batch = PickingBatch.create({'user_id': user_id})
+        picking.batch_id = batch.id
+        batch.write({'state': 'in_progress'})
+
+        return batch
+
+    def _check_batches(self, user_id):
+        """
+        Check ...
+        """
+        Picking = self.env['stock.picking']
+        PickingBatch = self.env['stock.picking.batch']
+
+        batches = PickingBatch.search([('user_id', '=', user_id),
+                                        ('state', '=', 'in_progress')])
+
+        if batches:
+            draft_picks = Picking.browse()
+            incomplete_picks = Picking.browse()
+
+            for pick in batches.mapped('picking_ids'):
+                if pick.state == 'draft':
+                    draft_picks += pick
+                elif pick.state not in ['done', 'cancel']:
+                    incomplete_picks += pick
+
+            if draft_picks:
+                draft_picks.write({'batch_id': None})
+
+            if incomplete_picks:
+                picks_txt = ','.join([x.name for x in incomplete_picks])
+                raise ValidationError(
+                    _("The user already has pickings that need completing - "
+                      "please complete those before requesting "
+                      "more:\n {}".format(picks_txt)))
+
+            # all the picks in the waves are finished
+            batches.done()
