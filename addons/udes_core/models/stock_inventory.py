42c969dadbb02ada618f4b53391163a642152aa5
parisiale <parisiale@gmail.com>
2018-03-15 08:36:11 +0000

    Extend StockInventory::action_done
    Adding check on blocked and undone inventories.

--- addons/udes_core/models/stock_inventory.py
+++ addons/udes_core/models/stock_inventory.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, models, fields
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
 
 
-class SotckInventory(models.Model):
+class StockInventory(models.Model):
     _name = 'stock.inventory'
     _inherit = 'stock.inventory'
 
@@ -16,3 +17,17 @@ class SotckInventory(models.Model):
                                           'Next inventory',
                                           readonly=True,
                                           index=True)
+
+    @api.multi
+    def action_done(self):
+        self.mapped('location_id').check_blocked(
+            prefix='Cannot validate inventory adjustment.')
+        self.mapped('line_ids.location_id').check_blocked(
+            prefix='Cannot validate inventory adjustment line.')
+
+        for prec in self.u_preceding_inventory_ids:
+            if prec.state != 'done':
+                raise ValidationError(
+                    _('There are undone preceding inventories.'))
+
+        return super(StockInventory, self).action_done()
