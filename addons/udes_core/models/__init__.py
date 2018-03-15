48bfa44bedb73f716561fc55b7b6ba16248aee12
parisiale <parisiale@gmail.com>
2018-03-15 08:39:15 +0000

    Extend StockProductionLot by adding get_lot method

--- addons/udes_core/models/__init__.py
+++ addons/udes_core/models/__init__.py
@@ -9,6 +9,7 @@ from . import stock_move_line
 from . import stock_picking
 from . import stock_picking_batch
 from . import stock_picking_type
+from . import stock_production_lot
 from . import stock_quant
 from . import stock_quant_package
 from . import stock_warehouse
