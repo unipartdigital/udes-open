9fc6053e41d2932c528c74494aaa2cd48d874f2a
parisiale <parisiale@gmail.com>
2018-03-15 08:41:14 +0000

    First cut of the inventory adjustment logic

--- addons/udes_core/models/stock_location.py
+++ addons/udes_core/models/stock_location.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from datetime import datetime
 
 from odoo import fields, models,  _
@@ -10,6 +10,12 @@ from odoo.exceptions import ValidationError
 PI_COUNT_MOVES = 'pi_count_moves'
 INVENTORY_ADJUSTMENTS = 'inventory_adjustments'
 PRECEDING_INVENTORY_ADJUSTMENTS = 'preceding_inventory_adjustments'
+
+NO_PACKAGE_TOKEN = 'NO_PACKAGE'
+NEW_PACKAGE_TOKEN = 'NEWPACKAGE'
+
+
+StockInfoPI = namedtuple('StockInfoPI', ['product_id', 'package_id', 'lot_id'])
 
 
 class StockLocation(models.Model):
@@ -149,26 +155,9 @@ class StockLocation(models.Model):
 
         return True
 
-    def _process_pi_count_moves(self, moves_request):
-        """
-            Returns the modified inventory in case changes were
-            necessary, None otherwise.
-        """
-        # @todo
-        pass
-
-    def _process_inventory_adjustments(self, adjustments_request):
-        """
-            Returns the modified inventory in case changes were
-            necessary, None otherwise.
-        """
-        # @todo
-        pass
-
     def _process_preceding_inventory_adjustments(self, pre_adj, next_adj_inv):
         Location = self.env['stock.location']
 
-        # @todo: do we need casting here?
         location_id = int(pre_adj['location_id'])
         location = Location.browse(location_id)
         other_adjs = pre_adj[INVENTORY_ADJUSTMENTS]
@@ -183,3 +172,98 @@ class StockLocation(models.Model):
                 for key in [PI_COUNT_MOVES, INVENTORY_ADJUSTMENTS]]):
             # No PI changes - the location is in a correct state
             self.write({'u_date_last_checked_correct': current_time})
+
+    def _process_pi_count_moves(self, moves_request):
+        """
+            Returns the modified inventory in case changes were
+            necessary, None otherwise.
+        """
+        # @todo
+        pass
+
+    def _process_inventory_adjustments(self, adjustments_request):
+        """
+            Returns the modified inventory in case changes were
+            necessary, None otherwise.
+        """
+        Inventory = self.env['stock.inventory']
+        InventoryLine = self.env['stock.inventory.line']
+
+        stock_drift = self._get_stock_drift(adjustments_request)
+
+        if not stock_drift:
+            return
+
+        inventory_adjustment = Inventory.create({
+            'name':        'PI inventory adjustment ' + self.name,
+            'location_id': self.id,
+            'filter':      'none',
+            'state':       'confirm'
+        })
+
+        for stock_info, quantity in stock_drift.items():
+            InventoryLine.create({
+              'inventory_id': inventory_adjustment.id,
+              'product_id':   stock_info.product_id,
+              'product_qty':  quantity,
+              'location_id':  self.id,
+              'package_id':   stock_info.package_id,
+              'prod_lot_id':  stock_info.lot_id})
+
+        return inventory_adjustment
+
+    def _get_stock_drift(self, adjustments_request):
+        """
+            Returns a dictionary where keys are StockInfo instances
+            and values are integers representing a product quantity.
+            Each StockInfo is the result of the processing of an
+            adjustments_request entry.
+
+            Creates a lot for a given product if necessary, when the
+            related lot name doesn't exist.
+
+            Raises a ValidationError in case any specified product
+            or package doesn't exist.
+        """
+        Product = self.env['product.product']
+        Package = self.env['stock.quant.package']
+        Lot = self.env['stock.production.lot']
+
+        stock_drift = {}
+        new_packages = {}
+
+        for adj in adjustments_request:
+            product = Product.get_product(int(adj['product_id']))
+
+            # determine the package
+
+            package_name = adj['package_name']
+            package = None
+
+            if NO_PACKAGE_TOKEN in package_name:
+                pass
+            elif NEW_PACKAGE_TOKEN in package_name:
+                if package_name in new_packages:
+                    package = new_packages[package_name]
+                else:
+                    package = Package.create({})
+                    new_packages[package_name] = package
+            else:
+                package = Package.get_package(package_name)
+
+            package_id = False if package is None else package.id
+
+            # determine the lot
+
+            lot_id = False
+
+            if product.tracking != 'none' and 'lot_name' in adj:
+                lot = Lot.get_lot(adj['lot_name'], product.id, create=True)
+                lot_id = lot.id
+
+            # add the entry
+
+            info = StockInfoPI(product.id, package_id, lot_id)
+            stock_drift[info] = adj['quantity']
+
+        return stock_drift
