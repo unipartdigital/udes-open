c9c3c81eef008145a411884079eaa1039950e7c8
Miquel PS <miquel.palahi.sitges@unipart.io>
2018-05-31 11:10:26 +0100

    Create empty module

---
+++ addons/udes_purchase/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "UDES Purchase",
+    'version': '11.0',
+    'summary': 'UDES Purchase',
+    'description': "UDES Purchase",
+    'author': 'Unipart Digital',
+    'category': 'Warehouse',
+    'depends': ['purchase'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
