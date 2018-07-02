e09d5c90b458680e528480b4d135cfc607be2f39
Michele Costa <michele.costa@unipart.io>
2018-07-02 17:54:34 +0100

    [MOV] udes_core: migrate stock.picking code from udes_api
    User-story: 1087
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

--- addons/udes_api/models/__init__.py
+++ addons/udes_api/models/__init__.py
@@ -2,6 +2,6 @@
 
 # from . import stock_location
 from . import stock_move_line
-from . import stock_picking
+#from . import stock_picking
 #from . import stock_picking_batch
 from . import stock_quant_package
--- addons/udes_core/models/stock_picking.py
+++ addons/udes_core/models/stock_picking.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
@@ -8,6 +8,25 @@ from odoo.exceptions import ValidationError, UserError
 from ..common import check_many2one_validity
 from . import common
 
+import logging
+_logger = logging.getLogger(__name__)
+
+
+
+def _update_move_lines_and_log_swap(move_lines, pack, other_pack):
+    """ Set the package ids of the specified move lines to the one
+        of the other package and signal the package swap by posting
+        a message to the picking instance of the first move line.
+
+        Assumes that both packages are singletons and that the
+        specified move lines are a non empty recordset.
+    """
+    move_lines.with_context(bypass_reservation_update=True)\
+              .write({"package_id": other_pack.id,
+                      "result_package_id": other_pack.id})
+    msg = _("Package %s swapped for package %s.") % (pack.name, other_pack.name)
+    move_lines[0].picking_id.message_post(body=msg)
+    _logger.info(msg)
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -160,27 +179,40 @@ class StockPicking(models.Model):
         """ By default allow to overreceive and it will be extended in
             a module where the picking type has a flag to decide this.
         """
+        Product = self.env['product.product']
+
         self.ensure_one()
-        old_move_lines = self.move_line_ids
-        self._create_moves(product_quantities, confirm=True, assign=True, unexpected=True)
-        new_move_lines = (self.move_line_ids - old_move_lines)
 
-        for ml in old_move_lines:
-            if ml.qty_done > 0 and ml.product_uom_qty > ml.qty_done:
-                # Note: when adding extra moves/move_lines, odoo will merge
-                # them into one of the existing ones, therefore we need
-                # to split the old_move_lines that have qty_done > 0 and
-                # qty_done < product_uom_qty
+        if self.picking_type_id.u_over_receive:
+            old_move_lines = self.move_line_ids
+            self._create_moves(product_quantities, confirm=True, assign=True, unexpected=True)
+            new_move_lines = (self.move_line_ids - old_move_lines)
 
-                # TODO: handle this properly at odoo core:
-                #       avoid to merge new move lines if qty_done > 0?
+            for ml in old_move_lines:
+                if ml.qty_done > 0 and ml.product_uom_qty > ml.qty_done:
+                    # Note: when adding extra moves/move_lines, odoo will merge
+                    # them into one of the existing ones, therefore we need
+                    # to split the old_move_lines that have qty_done > 0 and
+                    # qty_done < product_uom_qty
 
-                # new_ml has qty_done 0 and ml is done
+                    # TODO: handle this properly at odoo core:
+                    #       avoid to merge new move lines if qty_done > 0?
 
-                new_ml = ml._split()
-                new_move_lines |= new_ml
-        # These are unexpected so the ordered_qty should be
-        new_move_lines.write({"ordered_qty": 0})
+                    # new_ml has qty_done 0 and ml is done
+                    new_ml = ml._split()
+                    new_move_lines |= new_ml
+
+            # These are unexpected so the ordered_qty should be
+            new_move_lines.write({"ordered_qty": 0})
+        else:
+            overreceived_qtys = ["%s: %s" % (Product.get_product(id).name, qty)
+                                 for id, qty in product_quantities.items()]
+            raise ValidationError(
+                    _("We are not expecting these extra quantities of"
+                      " these parts:\n%s\nPlease either receive the right"
+                      " amount and move the rest to probres, or if they"
+                      " cannot be split, move all to probres.") %
+                    '\n'.join(overreceived_qtys))
 
         return new_move_lines
 
@@ -364,8 +396,9 @@ class StockPicking(models.Model):
             move_parent_package=False,
             product_ids=None,
             picking_info=None,
-            validate_real_time=False,
+            validate_real_time=None,
             location_id=None,
+            **kwargs
     ):
         """ Update/mutate the stock picking in self
 
@@ -408,7 +441,25 @@ class StockPicking(models.Model):
         """
         Location = self.env['stock.location']
         Package = self.env['stock.quant.package']
+
         self.assert_valid_state()
+
+        # If transport data has been sent in kwargs, add it to picking_info
+        if 'u_transport_id' in kwargs:
+            # retrieve transport info changes, and then remove them from kwargs
+            transport_id = kwargs.pop('u_transport_id')
+            # if picking_info already exists update with trailer info
+            if picking_info is None:
+                picking_info = transport_id
+            else:
+                picking_info.update(transport_id)
+
+        if validate_real_time is None:
+            validate_real_time = self.picking_type_id.u_validate_real_time
+
+        if 'expected_package_name' in kwargs:
+            self = self.with_context(
+                expected_package_name=kwargs.pop('expected_package_name'))
 
         values = {}
 
@@ -854,3 +905,222 @@ class StockPicking(models.Model):
                 pick.message_post(body=text)
 
             pick.do_unreserve()
+
+    def action_confirm(self):
+        """
+            Override action_confirm to create procurement groups if needed
+        """
+        for pick in self.filtered(
+                lambda p: p.picking_type_id.u_create_procurement_group
+                          and not p.group_id):
+            pick._create_own_procurement_group()
+        super(StockPicking, self).action_confirm()
+
+    def action_assign(self):
+        """
+            Override action_assign to reserve full packages if applicable
+        """
+        super(StockPicking, self).action_assign()
+        self._reserve_full_packages()
+
+    def _check_entire_pack(self):
+        """
+            Override to avoid values at result_package_id when
+            user scans products
+        """
+        pickings = self.filtered(lambda p: p.picking_type_id.u_user_scans != 'product')
+        super(StockPicking, pickings)._check_entire_pack()
+
+    def _reserve_full_packages(self):
+        """
+            If the picking type of the picking in self has full package
+            reservation enabled, partially reserved packages are
+            completed.
+        """
+        Quant = self.env['stock.quant']
+        MoveLine = self.env['stock.move.line']
+
+        # do not reserve full packages when bypass_reserve_full packages
+        # is set in the context as True
+        if not self.env.context.get('bypass_reserve_full_packages'):
+            for picking in self:
+                # Check if the picking type requires full package reservation
+                if picking.picking_type_id.u_reserve_as_packages:
+                    all_quants = Quant.browse()
+                    remaining_qtys = defaultdict(int)
+
+                    # get all packages
+                    packages = self.mapped('move_line_ids.package_id')
+                    for package in packages:
+                        move_lines = self.mapped('move_line_ids').filtered(lambda ml: ml.package_id == package)
+                        # TODO: merge with assert_reserved_full_package
+                        pack_products = frozenset(package._get_all_products_quantities().items())
+                        mls_products = frozenset(move_lines._get_all_products_quantities().items())
+                        if pack_products != mls_products:
+                            # move_lines do not match the quants
+                            pack_mls = MoveLine.search([('package_id', 'child_of', package.id),
+                                                        ('state', 'not in', ['done', 'cancel'])
+                                                        ])
+                            other_pickings = pack_mls.mapped('picking_id') - picking
+                            if other_pickings:
+                                raise ValidationError(
+                                    _('The package is reserved in other pickings: %s')
+                                    % ','.join(other_pickings.mapped('name')))
+
+                            quants = package._get_contained_quants()
+                            all_quants |= quants
+                            for product, qty in quants.group_quantity_by_product(only_available=True).items():
+                                remaining_qtys[product] += qty
+                    if remaining_qtys:
+                        # Context variables:
+                        # - filter the quants used in _create_moves() to be
+                        # the ones of the packages to be completed
+                        # - add bypass_reserve_full_packages at the context
+                        # to avoid to be called again inside _create_moves()
+                        picking.with_context(
+                            bypass_reserve_full_packages=True,
+                            quant_ids=all_quants.ids)._create_moves(remaining_qtys,
+                                                                confirm=True,
+                                                                assign=True)
+
+
+    def _create_own_procurement_group(self):
+        """Create a procurement group for self with the same name as self."""
+        self.ensure_one()
+        group = self.env['procurement.group'].create({'name': self.name})
+        self.move_lines.write({'group_id': group.id})
+
+
+    def _swap_package(self, scanned_package, expected_package,
+                      scanned_pack_mls, exp_pack_mls):
+        """ Performs the swap. """
+        if scanned_pack_mls and exp_pack_mls:
+            # Both packages are in move lines; we simply change
+            # the package ids of both scanned and expected move lines
+            _update_move_lines_and_log_swap(scanned_pack_mls,
+                                            scanned_package,
+                                            expected_package)
+            _update_move_lines_and_log_swap(exp_pack_mls,
+                                            expected_package,
+                                            scanned_package)
+        else:
+            assert exp_pack_mls is not None, \
+                "Expected package move lines empty"
+
+            # We know that scanned_pack_mls is empty; we should now
+            # 1) unreserve quants of the expected one, 2) reserve quants
+            # of the scanned package, and 3) change the package ids of
+            # the expected move lines
+            expected_package._get_contained_quants()\
+                            .write({'reserved_quantity': 0})
+
+            for q in scanned_package._get_contained_quants():
+                q.write({'reserved_quantity': q.quantity})
+
+            _update_move_lines_and_log_swap(exp_pack_mls,
+                                            expected_package,
+                                            scanned_package)
+
+        return exp_pack_mls
+
+    def maybe_swap(self, scanned_package, expected_package):
+        """ Validate the conditions for performing a swap of the
+            specified packages by considering the picking instance
+            (expects a singleton picking) and relevant move lines.
+
+            Return the move lines related to the expected package,
+            in case a swap is performed, or the ones related to the
+            scanned package, if both packages belong to the same
+            batch.
+
+            Raise a ValidationError in case packages cannot be found
+            in the picking or if the conditions for swapping are not
+            met.
+
+        """
+        self.ensure_one()
+
+        if not self.picking_type_id.u_allow_swapping_packages:
+            raise ValidationError(
+                _("Cannot swap packages of picking type '%s'")
+                % self.picking_type_id.name)
+
+        exp_pack_mls = self.move_line_ids.filtered(
+            lambda ml: ml.package_id == expected_package)
+
+
+        if not exp_pack_mls:
+            raise ValidationError(
+                _("Expected package cannot be found in picking %s") %
+                self.name)
+
+        if not scanned_package.has_same_content(expected_package):
+            raise ValidationError(
+                _("The contents of %s does not match what you have been "
+                  "asked to pick.") % expected_package.name)
+
+        if scanned_package.location_id != expected_package.location_id:
+            raise ValidationError(
+                _("Packages are in different locations and cannot be swapped"))
+
+        scanned_pack_mls = None
+
+        if scanned_package.is_reserved():
+            scanned_pack_mls = scanned_package.find_move_lines(
+                [('qty_done', '=', 0)])
+
+            if scanned_pack_mls:
+                # We know that all the move lines have the same picking id
+                mls_picking = scanned_pack_mls[0].picking_id
+
+                if mls_picking.picking_type_id != self.picking_type_id:
+                    raise ValidationError(
+                        _("Packages have different picking types and cannot "
+                          "be swapped"))
+
+                if self.batch_id \
+                   and mls_picking.batch_id == self.batch_id:
+                    # The scanned package and the expected are in
+                    # the same batch; don't need to be swapped -
+                    # simply return the move lines of the scanned
+                    # package
+                    return scanned_pack_mls
+                else:
+                    # We should swap the packages...
+                    pass
+
+        return self._swap_package(scanned_package, expected_package,
+                                  scanned_pack_mls, exp_pack_mls)
+
+    def _get_package_search_domain(self, package):
+        """ Override to handle multiple levels of packages
+        """
+        return ['|', ('move_line_ids.package_id', 'child_of', package.id),
+                '|', ('move_line_ids.result_package_id', 'child_of', package.id),
+                ('move_line_ids.u_result_parent_package_id', '=', package.id)]
+
+    def is_compatible_package(self, package_name):
+        """ The package with name package_name is compatible
+            with the picking in self if:
+            - The package does not exist
+            - The package is not in stock
+            - The package has not been used in any other picking
+        """
+        Picking = self.env['stock.picking']
+        Package = self.env['stock.quant.package']
+
+        self.ensure_one()
+        self.assert_valid_state()
+
+        res = True
+        pickings = Picking.get_pickings(package_name=package_name)
+        if len(pickings) == 0:
+            package = Package.get_package(package_name, no_results=True)
+            if package and package.quant_ids or package.children_ids:
+                # the package exists and it contains stock or other packages
+                res = False
+        elif len(pickings) > 1 or (len(pickings) == 1 and self != pickings):
+            # the package has been used
+            res = False
+
+        return res
