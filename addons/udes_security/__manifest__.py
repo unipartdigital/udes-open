733544e3b411e49500b126d268ad5fbb847507a3
Michael Brown <mbrown@fensystems.co.uk>
2018-07-24 01:07:23 +0100

    [ADD] udes_security: Add udes_security module and open redirect filter
    Block absolute redirections via the /web and /web/login HTTP routes.
    
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ addons/udes_security/__manifest__.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-docstring,pointless-statement
+{
+    'name': "UDES Security",
+    'summary': "UDES security enhancements",
+    'description': """
+Security enhancements used by UDES
+==================================
+""",
+    'version': '0.1',
+    'depends': [
+        'web',
+    ],
+    'category': "Authentication",
+}
---
+++ addons/udes_security/controllers/redirect.py
@@ -0,0 +1,60 @@
+"""Open redirect filter"""
+
+import logging
+from werkzeug import urls
+from werkzeug.exceptions import BadRequest
+from odoo import http
+from odoo.tools.translate import _
+from odoo.addons.web.controllers import main
+
+_logger = logging.getLogger(__name__)
+
+
+class no_absolute_redirect(object):
+    """Decorator to mark a route as forbidding absolute redirects
+
+    If the decorated route is called with a "redirect" query parameter
+    containing an absolute URL, the request will be terminated with an
+    exception.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    @property
+    def __name__(self):
+        """Copy original method name
+
+        This is required since the method name will be introspected as
+        part of calculating the controller inheritance structure.
+        """
+        return self.func.__name__
+
+    def __call__(self, *args, **kwargs):
+        """Call method"""
+        redirect = kwargs.get('redirect')
+        if redirect is not None and self.is_absolute_url(redirect):
+            _logger.error("Invalid redirection attempted: %s", redirect)
+            raise BadRequest(_("Invalid redirection attempted"))
+        return self.func(*args, **kwargs)
+
+    @staticmethod
+    def is_absolute_url(url):
+        """Test if a URL is an absolute URL"""
+        return bool(urls.url_parse(url).scheme)
+
+
+class Home(main.Home):
+    """Web client controller"""
+
+    @http.route()
+    @no_absolute_redirect
+    def web_client(self, *args, **kwargs):
+        """Prevent absolute redirects on /web"""
+        return super().web_client(*args, **kwargs)
+
+    @http.route()
+    @no_absolute_redirect
+    def web_login(self, *args, **kwargs):
+        """Prevent absolute redirects on /web/login"""
+        return super().web_login(*args, **kwargs)
