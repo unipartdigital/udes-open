696ffb8d0645aac2bf170cc0f7199bc932d49d97
parisiale <parisiale@gmail.com>
2018-10-05 13:27:42 +0100

    [FIX] Trivial style fixes

--- addons/udes_stock/controllers/stock_picking.py
+++ addons/udes_stock/controllers/stock_picking.py
@@ -9,7 +9,8 @@ from .main import UdesApi
 
 class PickingApi(UdesApi):
 
-    @http.route('/api/stock-picking/', type='json', methods=['GET'], auth='user')
+    @http.route('/api/stock-picking/',
+                type='json', methods=['GET'], auth='user')
     def get_pickings(self, fields_to_fetch=None, **kwargs):
         """ Search for pickings by various criteria and return an
             array of stock.picking objects that match a given criteria.
@@ -26,26 +27,33 @@ class PickingApi(UdesApi):
 
         return pickings.get_info(fields_to_fetch=fields_to_fetch)
 
-    @http.route('/api/stock-picking/', type='json', methods=['POST'], auth='user')
+    @http.route('/api/stock-picking/',
+                type='json', methods=['POST'], auth='user')
     def create_picking(self, **kwargs):
         """ Old create_internal_transfer
         """
         Picking = request.env['stock.picking']
         picking = Picking.create_picking(**kwargs)
+
         return picking.get_info()[0]
 
-    @http.route('/api/stock-picking/<ident>', type='json', methods=['POST'], auth='user')
+    @http.route('/api/stock-picking/<ident>',
+                type='json', methods=['POST'], auth='user')
     def update_picking(self, ident, **kwargs):
         """ Old force_validate/validate_operation
         """
         Picking = request.env['stock.picking']
         picking = Picking.browse(int(ident))
+
         if not picking.exists():
             raise ValidationError(_('Cannot find stock.picking with id %s') % ident)
+
         picking.update_picking(**kwargs)
+
         return picking.get_info()[0]
 
-    @http.route('/api/stock-picking/<ident>/is_compatible_package/<package_name>', type='json', methods=['GET'], auth='user')
+    @http.route('/api/stock-picking/<ident>/is_compatible_package/<package_name>',
+                type='json', methods=['GET'], auth='user')
     def is_compatible_package(self, ident, package_name):
         """ Check if the package name is compatible with the
             picking with id <ident>, i.e., the package name has not been
@@ -54,8 +62,10 @@ class PickingApi(UdesApi):
         """
         Picking = request.env['stock.picking']
         picking = Picking.browse(int(ident))
+
         if not picking.exists():
             raise ValidationError(_('Cannot find stock.picking with id %s') % ident)
+
         return picking.is_compatible_package(package_name)
 
     @http.route('/api/stock-picking/<identifier>/suggested-locations',
