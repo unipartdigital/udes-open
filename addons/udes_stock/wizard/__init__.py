d403b60c18670e03884747ef749a1711cacb9536
Charlie Wheeler-Robinson <crwr@pigotts.org.uk>
2018-10-24 16:38:31 +0100

    [IMP] udes_stock: Add wizards to trigger pick/move refactor from web UI
    This allows the refactoring to be manually triggered on any set of
    pickings or moves in order to recover from errors, or for cases when
    automatic triggering is insufficient.
    
    Story: 1942
    Signed-off-by: Charlie Wheeler-Robinson <crwr@pigotts.org.uk>

--- addons/udes_stock/wizard/__init__.py
+++ addons/udes_stock/wizard/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import change_quant_location
+from . import refactor_views
---
+++ addons/udes_stock/wizard/refactor_views.py
@@ -0,0 +1,40 @@
+from odoo import api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class RefactorStockPicking(models.TransientModel):
+    _name = 'stock.picking.refactor.wizard'
+    _description = 'Trigger refactoring of pickings from the UI'
+
+    @api.multi
+    def do_refactor(self):
+        Picking = self.env['stock.picking']
+        self.ensure_one()
+        picking_ids = self.env.context.get('active_ids')
+
+        _logger.info("User number {UID} has requested refactoring of pickings "
+                     "{PICKINGS}".format(UID=str(self.env.uid),
+                                         PICKINGS=picking_ids))
+
+        res = Picking.browse(picking_ids).mapped('move_lines')._action_refactor()
+        return res
+
+
+class RefactorStockMove(models.TransientModel):
+    _name = 'stock.move.refactor.wizard'
+    _description = 'Trigger refactoring of moves from the UI'
+
+    @api.multi
+    def do_refactor(self):
+        Move = self.env['stock.move']
+        self.ensure_one()
+        move_ids = self.env.context.get('active_ids')
+
+        _logger.info("User number {UID} has requested refactoring of moves "
+                     "{MOVES}".format(UID=str(self.env.uid),
+                                      MOVES=move_ids))
+
+        res = Move.browse(move_ids)._action_refactor()
+        return res
