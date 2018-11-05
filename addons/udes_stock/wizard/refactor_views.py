7d939722e3425f1d654cb0bed20fcc25fbc4bb83
Sam Clark <sam.clark@unipart.io>
2018-11-05 11:16:02 +0000

    [ADD] udes_stock: Add refactor pickings to batch list view

--- addons/udes_stock/wizard/refactor_views.py
+++ addons/udes_stock/wizard/refactor_views.py
@@ -38,3 +38,20 @@ class RefactorStockMove(models.TransientModel):
 
         res = Move.browse(move_ids)._action_refactor()
         return res
+
+class RefactorStockPickingBatch(models.TransientModel):
+    _name = 'stock.picking.batch.refactor.wizard'
+    _description = 'Trigger refactoring of batches from the UI'
+
+    @api.multi
+    def do_refactor(self):
+        Batch = self.env['stock.picking.batch']
+        self.ensure_one()
+        batch_ids = self.env.context.get('active_ids')
+
+        _logger.info("User number {UID} has requested refactoring of batches "
+                     "{BATCHES}".format(UID=str(self.env.uid),
+                                      BATCHES=batch_ids))
+
+        res = Batch.browse(batch_ids).mapped('picking_ids.move_lines')._action_refactor()
+        return res        
