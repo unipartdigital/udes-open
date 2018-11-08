b0861dc211e4ea920c48327178d4782c5883b65a
David Spence <david.spence@unipart.io>
2018-11-08 17:21:19 +0000

    [WIP] print strategy for stock picking by type
    Unit tests fail with an unknown savepoint error on rollback.
    
    Signed-off-by: David Spence <david.spence@unipart.io>

--- addons/udes_stock/models/__init__.py
+++ addons/udes_stock/models/__init__.py
@@ -16,6 +16,7 @@ from . import stock_move_line
 from . import stock_picking
 from . import stock_picking_batch
 from . import stock_picking_type
+from . import stock_picking_print_strategy
 from . import stock_production_lot
 from . import stock_quant
 from . import stock_quant_package
