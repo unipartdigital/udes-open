0b20bff9c89319f688a9a7b67fffc57e105264a5
rjw <richard.whitefoot@unipart.io>
2018-12-11 19:17:31 +0000

    Fix: To rebase

--- addons/udes_sale_stock/models/res_partner.py
+++ addons/udes_sale_stock/models/res_partner.py
@@ -8,14 +8,12 @@ class Partner(models.Model):
     _inherit = "res.partner"
 
     @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    def search(self, args, offset=0, limit=None, order=None, count=False):
 
-        # Rules does not apply to administrator, group_inbound_user do not see customers
+        # Rule does not apply to administrator, group_inbound_user do not see customers
         if self._uid != SUPERUSER_ID and self.env['res.users'].has_group('udes_stock.group_inbound_user'):
             args = ['&', ('customer', '=', False)] + list(args)
 
             _logger.info('udes_stock.group_inbound_user search limited to none customers')
         
-        return super(Partner, self)._search(
-            args, offset=offset, limit=limit, order=order,
-            count=False, access_rights_uid=access_rights_uid)
+        return super(Partner, self).search(args, offset, limit, order, count=count)
