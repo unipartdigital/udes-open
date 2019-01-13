c37b9dcb81625db13b0a4e31a9afeb786d9c2f1f
parisiale <parisiale@gmail.com>
2019-01-13 20:36:48 +0000

    [IMP] Trivial style fixes
    User-story: 2557

--- addons/udes_sale_stock/models/partner_pii.py
+++ addons/udes_sale_stock/models/partner_pii.py
@@ -7,32 +7,31 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
+REDACTED_TXT = _('Redacted')
 PII_RELATIONS = [
-    'partner_id', 'res_partner_id', 'author_id', 'partner_address_id', 'owner_id', 
-    'partner_shipping_id', 'partner_invoice_id', 'order_partner_id', 'customer_id'
+    'partner_id', 'res_partner_id', 'author_id', 'partner_address_id',
+    'owner_id', 'partner_shipping_id', 'partner_invoice_id',
+    'order_partner_id', 'customer_id'
 ]
 
 
 @api.model
 def _read(self, result):
     """Redact fields based on res.user.u_view_customers"""
-
     if self.env.uid == SUPERUSER_ID or self.env.user.u_view_customers:
         return result
-    
+
     for record in result:
-                  
         for field in record:
-            
-            if (field in PII_RELATIONS \
-                and isinstance(record[field], tuple) \
-                and self.env['res.partner'].sudo().search_count([
-                    ('id', '=', record[field][0]), 
-                    ('customer', '=', True)
-                    ])):
-                        # Record ID remains and string value redacted
-                        record[field] = (record[field][0], _('Redacted'))
-                        _logger.info("Redacted: %s.%s", self._name, field)
+            if (field in PII_RELATIONS
+                    and isinstance(record[field], tuple)
+                    and self.env['res.partner'].sudo().search_count([
+                        ('id', '=', record[field][0]),
+                        ('customer', '=', True)])):
+                # Record ID remains and string value redacted
+                record[field] = (record[field][0], REDACTED_TXT)
+                _logger.info("%s: %s.%s", REDACTED_TXT, self._name, field)
 
     return result
 
@@ -42,24 +41,27 @@ class ResPartner(models.Model):
 
     @api.multi
     def name_get(self):
-        """We rely on the ACL to limit access to this model, but we still need to redact data retrieved via joined queries"""
-
+        """We rely on the ACL to limit access to this model, but we
+           still need to redact data retrieved via joined queries
+        """
         result = super(ResPartner, self).name_get()
 
         if self.env.uid == SUPERUSER_ID or self.env.user.u_view_customers:
             return result
 
-        for i, record in enumerate(result):
-            if (isinstance(record, tuple) \
-                and self.sudo().search_count([
-                    ('id', '=', record[0]), 
-                    ('customer', '=', True)
-                    ])):
-                        # Record ID remains and string value redacted
-                        result[i] = (record[0], _('Redacted'))
-                        _logger.info("Redacted: res.partner")
+        redacted_result = []
 
-        return result
+        for record in result:
+            if (isinstance(record, tuple)
+                    and self.sudo().search_count([('id', '=', record[0]),
+                                                  ('customer', '=', True)])):
+                # Record ID remains and string value redacted
+                redacted_result.append((record[0], REDACTED_TXT))
+                _logger.info(REDACTED_TXT + ": res.partner")
+            else:
+                redacted_result.append(record)
+
+        return redacted_result
 
 
 class SaleOrder(models.Model):
@@ -67,9 +69,8 @@ class SaleOrder(models.Model):
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
-
         result = super(SaleOrder, self).read(fields=fields, load=load)
-        return _read(self, result)    
+        return _read(self, result)
 
 
 class StockPicking(models.Model):
@@ -77,7 +78,5 @@ class StockPicking(models.Model):
 
     @api.multi
     def read(self, fields=None, load='_classic_read'):
-
         result = super(StockPicking, self).read(fields=fields, load=load)
         return _read(self, result)
-
