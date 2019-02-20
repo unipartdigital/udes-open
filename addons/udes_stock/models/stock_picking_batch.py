f25a205d7c61953b3708437b4e028182eb6c38f6
richardwhitefoot <richard.whitefoot@unipart.io>
2019-02-20 22:09:39 +0000

    [IMP] udes_stock: Run drop off picked as sudo user for performance gain
    Signed-off-by: Richard Whitefoot <richard.whitefoot@unipart.io>

--- addons/udes_stock/models/stock_picking_batch.py
+++ addons/udes_stock/models/stock_picking_batch.py
@@ -499,7 +499,7 @@ class StockPickingBatch(models.Model):
             # Add backorders to the batch
             to_add.write({'batch_id': self.id})
             with self.statistics() as stats:
-                picks_todo.with_context(tracking_disable=True).action_done()
+                picks_todo.sudo().with_context(tracking_disable=True).action_done()
             _logger.info("%s action_done in %.2fs, %d queries",
                          picks_todo, stats.elapsed, stats.count)
 
