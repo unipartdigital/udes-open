6a1bf04cd04313ce71b3b3bcb902426b5a0b4531
Miquel PS <miquel.palahi.sitges@unipart.io>
2019-03-26 09:03:27 +0000

    [IMP] udes_purchase: Filter order point domain by active_ids
    This allows the user to select for which order points wants to
    check if reordering is required.
    
    User-story: 3486
    
    Signed-off-by: Miquel PS <miquel.palahi.sitges@unipart.io>

--- addons/udes_purchase/models/procurement.py
+++ addons/udes_purchase/models/procurement.py
@@ -26,3 +26,12 @@ class ProcurementGroup(models.Model):
                 except Exception:
                     pass
         return {}
+
+    def _get_orderpoint_domain(self, company_id=False):
+        """ Override order point domain to filter by active_ids
+        """
+        domain = super()._get_orderpoint_domain(company_id=company_id)
+        orderpoints = self.env.context.get('active_ids', None)
+        if orderpoints:
+            domain += [('id', 'in', orderpoints)]
+        return domain
