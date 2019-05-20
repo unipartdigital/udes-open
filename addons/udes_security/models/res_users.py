ff08eb9583e424d9643adb664d97f86292126332
Sean Quah <sean.quah@unipart.io>
2019-05-20 20:46:54 +0100

    [FIX] udes_security: Fix Trusted User write check
    Fix the res.users write override to correctly handle all many2many write
    operations. Also remove the assumption that many2many write operations have
    the right number of elements and no more.
    
    Task: 4209
    User-story: 3917
    
    Signed-off-by: Sean Quah <sean.quah@unipart.io>

--- addons/udes_security/models/res_users.py
+++ addons/udes_security/models/res_users.py
@@ -30,7 +30,30 @@ class Users(models.Model):
         values = self._remove_reified_groups(values)
 
         # Only the root user can add or remove the Trusted User group
-        for op, group_id in values.get('groups_id', []):
-            if (group_id == group_trusted_user.id and
-                self.env.uid != SUPERUSER_ID):
+        if (self.env.uid != SUPERUSER_ID and
+           'groups_id' in values):
+            # Validate the many2many write to check that the
+            # Trusted User group is not being added or removed.
+            disallowed = False
+            for act in values['groups_id']:
+                if act[0] in (0, 1, 2):
+                    pass
+                elif act[0] in (3, 4):
+                    # Remove and add group
+                    disallowed |= act[1] == group_trusted_user.id
+                elif act[0] == 5:
+                    # Remove all groups
+                    disallowed |= group_trusted_user in self.mapped('groups_id')
+                elif act[0] == 6:
+                    # Replace all groups
+                    for user in self:
+                        disallowed |= ((group_trusted_user in user.groups_id) !=
+                                       (group_trusted_user.id in act[2]))
+                else:
+                    raise ValueError('Unknown operation in many2many write for groups_id')
+
+                if disallowed:
+                    break
+
+            if disallowed:
                 raise AccessError(_('Trusted Users cannot be added or removed due to security restrictions. Please contact your system administrator.'))
