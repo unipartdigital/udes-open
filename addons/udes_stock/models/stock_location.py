caf9873dfb454a1c0d43bc5153214efa069b59f2
Robert Smith <robert.smith@unipart.io>
2019-06-04 12:06:34 +0100

    [ADD] hds: Check for presence of forbidden package name for PI
    For PI a forbidden package name can be specified in the front end
    which is used in the backend to not generate a package but simply
    count the amout of product found during the PI process.
    
    Epic: 4189
    Story: 3886
    Signed-off-by: Robert Smith <robert.smith@unipart.io>

--- addons/udes_stock/models/stock_location.py
+++ addons/udes_stock/models/stock_location.py
@@ -428,7 +428,7 @@ class StockLocation(models.Model):
         Product = self.env['product.product']
         Package = self.env['stock.quant.package']
         Lot = self.env['stock.production.lot']
-
+        wh = self.env.user.get_user_warehouse()
         stock_drift = {}
         new_packages = {}
 
@@ -439,8 +439,8 @@ class StockLocation(models.Model):
 
             package_name = adj['package_name']
             package = None
-
-            if NO_PACKAGE_TOKEN in package_name:
+            if NO_PACKAGE_TOKEN in package_name \
+                or package_name in wh.forbidden_package_names:
                 pass
             elif NEW_PACKAGE_TOKEN in package_name:
                 if package_name in new_packages:
