718aceb54006495585bde2d41ddfa45451a27aa1
Joe <joe.burgess@unipart.io>
2019-09-06 18:47:55 +0100

    [FIX] udes_sale_stock: Default should be 1 day ahead not forever

--- addons/udes_sale_stock/models/stock_warehouse.py
+++ addons/udes_sale_stock/models/stock_warehouse.py
@@ -6,7 +6,7 @@ class StockWarehouse(models.Model):
 
     u_so_auto_confirm_ahead_days = fields.Integer(
         "Auto Confirm sale order within (days)",
-        default=-1,
+        default=1,
         help="Days ahead to auto confirm sales orders for. "
              "0 = Today, 1 = Tomorrow, -1 = All"
     )
