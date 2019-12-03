9ced1ba083739828efcc305cf3b91e1bc57d665c
Michele Costa <michele.costa@unipart.io>
2019-12-03 14:32:51 +0000

    [ADD] Add inital udes_common
    Signed-off-by: Michele Costa <michele.costa@unipart.io>

---
+++ addons/udes_common/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "udes_common",
+    "summary": "Common componets for udes",
+    "description": "Common componets for udes",
+    "author": "Unipart digital",
+    "website": "http://github/unipartdigital/udes-open",
+    "category": "UDES",
+    "version": "0.1",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+}
