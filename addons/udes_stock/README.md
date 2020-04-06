e31e8daa4eb456b7eacb4714a7bb968d8720e515
Petepratt <peter.pratt@unipart.io>
2020-04-06 14:13:00 +0100

    [ADD] udes_stock_13: Basic Warehouse configuration
    User-story: 9844
    
    README

---
+++ addons/udes_stock/README.md
@@ -0,0 +1,1 @@
+# UDES Stock
---
+++ addons/udes_stock/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "udes_stock",
+    "summary": "udes stock",
+    "description": "Core models and configuration for udes stock - odoo 13",
+    "author": "Unipart digital",
+    "website": "http://github/unipartdigital/udes-open",
+    "category": "UDES",
+    "version": "0.1",
+    "depends": [
+        'base',
+    ],
+    'data': [],
+}
