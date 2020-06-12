5f97feff562a0c4197c2b3b5e3a7de4556688d34
Peter Clark <peter.clark@unipart.io>
2020-06-12 15:28:09 +0100

    [IMP] udes_security: Black formatting run on manifest file
    Story/9770
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_security/__manifest__.py
+++ addons/udes_security/__manifest__.py
@@ -1,27 +1,22 @@
 # pylint: disable=missing-docstring,pointless-statement
 {
-    'name': "UDES Security",
-    'summary': "UDES security enhancements",
-    'description': """
+    "name": "UDES Security",
+    "summary": "UDES security enhancements",
+    "description": """
 Security enhancements used by UDES
 ==================================
 """,
-    'version': '0.1',
-    'depends': [
-        'web',
-        'password_security',
-        'auth_brute_force',
-        'auth_session_timeout',
+    "version": "0.1",
+    "depends": ["web", "password_security", "auth_brute_force", "auth_session_timeout"],
+    "category": "Authentication",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/blocked_file_type_views.xml",
+        "views/res_users_views.xml",
+        "views/webclient_templates.xml",
+        "data/auth_brute_force.xml",
+        "data/ir_config_parameter_data.xml",
+        "data/res_groups.xml",
+        "data/res_users.xml",
     ],
-    'category': "Authentication",
-    'data': [
-        'security/ir.model.access.csv',
-        'views/blocked_file_type_views.xml',
-        'views/res_users_views.xml',
-        'views/webclient_templates.xml',
-        'data/auth_brute_force.xml',
-        'data/ir_config_parameter_data.xml',
-        'data/res_groups.xml',
-        'data/res_users.xml',
-    ]
 }
