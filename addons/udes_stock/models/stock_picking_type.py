55f8aa612579295f0b8fb3efdca7ecb9a2c3de4d
Peter Clark <peter.clark@unipart.io>
2020-06-25 13:10:02 +0100

    [ADD] udes_stock: Auto Unlink Empty boolean field added to Stock Picking Type
    Story/9859
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_stock/models/stock_picking_type.py
+++ addons/udes_stock/models/stock_picking_type.py
@@ -1,8 +1,15 @@
-from odoo import models
+from odoo import models, fields
 
 
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
+
+    u_auto_unlink_empty = fields.Boolean(
+        string="Auto Unlink Empty",
+        default=True,
+        help="Flag to indicate whether to unlink empty pickings when searching "
+        " for any empty picking in the system.",
+    )
 
     def get_action_picking_tree_draft(self):
         return self._get_action("udes_stock.action_picking_tree_draft")
