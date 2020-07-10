84261a12d0c9114ef4660ef949e01304db814d6f
Peter Clark <peter.clark@unipart.io>
2020-07-10 09:13:29 +0100

    [IMP] udes_stock_refactoring: Refactor abstract class now uses RegistryMeta from udes_common
    Story/9859
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_stock_refactoring/models/__init__.py
+++ addons/udes_stock_refactoring/models/__init__.py
@@ -5,7 +5,6 @@ from . import stock_move_line
 from . import stock_picking
 from . import stock_picking_batch
 from . import stock_picking_type
-from . import registry
 from . import refactor
 from . import refactor_group_by_move_line_key
 from . import refactor_group_by_move_key
