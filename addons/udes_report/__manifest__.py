06d808d0b71181ae478c1c0284de4cde4953c01a
ajrwilliams <anthony.williams@unipart.io>
2020-07-24 15:12:25 +0100

    [IMP] udes_report: Black formatting
    User-story: 10704
    Signed-off-by: ajrwilliams <anthony.williams@unipart.io>

--- addons/udes_report/__manifest__.py
+++ addons/udes_report/__manifest__.py
@@ -1,33 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "UDES Report",
-
-    'summary': """
+    "name": "UDES Report",
+    "summary": """
         Creates and exports stock reports
         """,
-
-    'author': "Unipart Digital",
-    'website': "http://unipart.io",
-
+    "author": "Unipart Digital",
+    "website": "http://unipart.io",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Specific Industry Applications',
-    'version': '0.1',
-
+    "category": "Specific Industry Applications",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base',
-                'stock',
-                'udes_stock'],
-
+    "depends": ["base",
+                "stock",
+                "udes_stock"],
     # always loaded
     # NB(ale): order is important for deps
-    'data': [
-        'views/menu.xml',
-        'views/export.xml',
-        'data/stock_email.xml',
-    ],
-
+    "data": ["views/menu.xml",
+             "views/export.xml",
+             "data/stock_email.xml",],
     # only loaded in demonstration mode
-    'demo': [],
+    "demo": [],
 }
--- addons/udes_report/models/stock_export.py
+++ addons/udes_report/models/stock_export.py
@@ -19,56 +19,58 @@ SAVE_DIR = "/home/odoo/"
 
 
 class StockExport(models.TransientModel):
-    _name = 'udes_report.stock_export'
-    _description = 'Creates spreadsheets summarising the warehouse stock'
+    _name = "udes_report.stock_export"
+    _description = "Creates spreadsheets summarising the warehouse stock"
 
-    file_name = fields.Char('Filename')
-    file_data = fields.Binary('File')
-    date = fields.Date('Date')
+    file_name = fields.Char("Filename")
+    file_data = fields.Binary("File")
+    date = fields.Date("Date")
 
     @api.model
     def __get_default_stock_location(self):
-        '''
+        """
         Propagates possible ValidationErrors if it fails to find
         the current user's warehouse.
-        '''
-        warehouse = self.env['res.users'].get_user_warehouse()
+        """
+        warehouse = self.env["res.users"].get_user_warehouse()
         return warehouse.lot_stock_id
 
     included_locations = fields.Many2many(
-        comodel_name='stock.location',
-        column1='included_locs',
-        column2='included_stock_reports',
-        relation='included_locations_rel',
-        string='Included Stock Locations',
-        help='Stock Locations to be included in the stock summary',
-        options={'no_create': True},
-        default=__get_default_stock_location)
+        comodel_name="stock.location",
+        column1="included_locs",
+        column2="included_stock_reports",
+        relation="included_locations_rel",
+        string="Included Stock Locations",
+        help="Stock Locations to be included in the stock summary",
+        options={"no_create": True},
+        default=__get_default_stock_location,
+    )
 
     excluded_locations = fields.Many2many(
-        comodel_name='stock.location',
-        column1='excluded_locs',
-        column2='excluded_stock_reports',
-        relation='excluded_locations_rel',
-        string='Excluded Stock Locations',
-        help='Stock Locations that will be excluded from the stock summary',
-        options={'no_create': True})
+        comodel_name="stock.location",
+        column1="excluded_locs",
+        column2="excluded_stock_reports",
+        relation="excluded_locations_rel",
+        string="Excluded Stock Locations",
+        help="Stock Locations that will be excluded from the stock summary",
+        options={"no_create": True},
+    )
 
-    def run_stock_file_export(self, context=None, send_file_via='user'):
-        '''
+    def run_stock_file_export(self, context=None, send_file_via="user"):
+        """
         Creates a stock file summarising the current stock in the
         warehouse. Such file will be in the excel format and contain
         two tabs:
             1) part number, location, package ID and quantity;
             2) part number, number of packages and total quantity.
-        '''
-        file_name = 'warehouse_stock_{TIMESTAMP}.xls'.format(TIMESTAMP=datetime.now())
+        """
+        file_name = "warehouse_stock_{TIMESTAMP}.xls".format(TIMESTAMP=datetime.now())
 
         data = self._generate_stock_file_data()
         self._write_workbook(file_name, "Stock File", data, send_file_via=send_file_via)
 
     def _generate_stock_file_data(self):
-        '''
+        """
         Generate data for the stock file.
 
         This will contain 2 sheets that may then be exported to any format
@@ -76,88 +78,96 @@ class StockExport(models.TransientModel):
         Sheets:
             1) part number, location, package ID and quantity;
             2) part number, number of packages and total quantity.
-        '''
-        Quant = self.env['stock.quant']
-        Product = self.env['product.product']
+        """
+        Quant = self.env["stock.quant"]
+        Product = self.env["product.product"]
 
         locations = self.included_locations - self.excluded_locations
         if not locations:
             raise UserError(_("The specified list of Stock Locations is empty."))
 
-        all_quants = Quant.search([('location_id', 'child_of', locations.ids)])
+        all_quants = Quant.search([("location_id", "child_of", locations.ids)])
 
         quants_by_prod_by_loc = OrderedDict()
-        prod_summary = defaultdict(lambda: {'n_pkgs': 0, 'qty': 0})
+        prod_summary = defaultdict(lambda: {"n_pkgs": 0, "qty": 0})
 
-        for prod, quants in all_quants.groupby('product_id'):
-            quants_by_prod_by_loc[prod] = {loc: grouped_quants
-                                           for loc, grouped_quants in quants.groupby("location_id")}
+        for prod, quants in all_quants.groupby("product_id"):
+            quants_by_prod_by_loc[prod] = {
+                loc: grouped_quants for loc, grouped_quants in quants.groupby("location_id")
+            }
 
         # 1/2) 'Stock File': one entry for each prod, package and lot (if applicable)
-        stock_file_data = {"name": 'Stock File'}
+        stock_file_data = {"name": "Stock File"}
         # Determine if lots/serials should be shown on the report
         show_tracking = bool(Product.search([("tracking", "!=", "none")], order="id", limit=1))
 
-        headers = ['Part Number', 'Location', 'Package', 'Quantity']
+        headers = ["Part Number", "Location", "Package", "Quantity"]
         if show_tracking:
-            headers.insert(3, 'Lot/Serial Number')
+            headers.insert(3, "Lot/Serial Number")
         stock_file_data["column_titles"] = headers
 
         rows = []
         for prod, quants_by_location in quants_by_prod_by_loc.items():
             for location, quants in quants_by_location.items():
-                for pkg_name, grouped_quants in quants.groupby(lambda x: x.package_id.name or ''):
-                    for lot_name, lot_grouped_quants in grouped_quants.groupby(lambda x:
-                                                                               x.lot_id.name or ''):
+                for pkg_name, grouped_quants in quants.groupby(lambda x: x.package_id.name or ""):
+                    for lot_name, lot_grouped_quants in grouped_quants.groupby(
+                        lambda x: x.lot_id.name or ""
+                    ):
                         qty = lot_grouped_quants.total_quantity()
-                        row = {'Part Number': prod.default_code,
-                               'Location': location.display_name,
-                               'Package': pkg_name,
-                               'Quantity': qty}
+                        row = {
+                            "Part Number": prod.default_code,
+                            "Location": location.display_name,
+                            "Package": pkg_name,
+                            "Quantity": qty,
+                        }
                         if show_tracking:
-                            row['Lot/Serial Number'] = lot_name
+                            row["Lot/Serial Number"] = lot_name
                         rows.append(row)
-                        prod_summary[prod]['qty'] += qty
+                        prod_summary[prod]["qty"] += qty
                     if pkg_name:
-                        prod_summary[prod]['n_pkgs'] += 1
-        stock_file_data['rows'] = rows
+                        prod_summary[prod]["n_pkgs"] += 1
+        stock_file_data["rows"] = rows
 
         # 2/2) 'Stock Summary': one entry for each prod
         stock_summary = {"name": "Stock Summary"}
-        stock_summary["column_titles"] = ['Part Number', 'Package Count', 'Quantity']
+        stock_summary["column_titles"] = ["Part Number", "Package Count", "Quantity"]
 
         rows = []
         for (prod, summary) in sorted(prod_summary.items(), key=lambda x: x[0].id):
-            rows.append({'Part Number': prod.default_code,
-                         'Package Count': summary['n_pkgs'],
-                         'Quantity': summary['qty']})
-        stock_summary['rows'] = rows
+            rows.append(
+                {
+                    "Part Number": prod.default_code,
+                    "Package Count": summary["n_pkgs"],
+                    "Quantity": summary["qty"],
+                }
+            )
+        stock_summary["rows"] = rows
 
         return [stock_file_data, stock_summary]
 
-    def run_movement_file_export(self, context=None, send_file_via='user'):
-        '''
+    def run_movement_file_export(self, context=None, send_file_via="user"):
+        """
         Creates movement file, in excel format, summarising the stock
         received and sent for a given date, i.e. goods-in and goods-out
         stock moves done, in two different tabs containing:
             1) Goods In:  Reference, Part number, Package, Quantity;
             2) Goods Out: Reference, Part number, Package, Quantity.
-        '''
-        file_name = 'warehouse_movement_{TIMESTAMP}.xls'.format(TIMESTAMP=datetime.now())
+        """
+        file_name = "warehouse_movement_{TIMESTAMP}.xls".format(TIMESTAMP=datetime.now())
 
         data = self._generate_movement_file_data()
         self._write_workbook(file_name, "Movement File", data, send_file_via=send_file_via)
 
     def _generate_movement_file_data(self):
-        '''
+        """
         Creates movement file data summarising the stock
         received and sent for a given date, i.e. goods-in and goods-out
         stock moves done, in two different tabs containing:
             1) Goods In:  Reference, Part number, Package, Quantity;
             2) Goods Out: Reference, Part number, Package, Quantity.
-        '''
-        Users = self.env['res.users']
-        Move = self.env['stock.move']
+        """
+        Users = self.env["res.users"]
+        Move = self.env["stock.move"]
 
         if not self.date:
             raise UserError(_("Date not specified."))
@@ -170,10 +180,14 @@ class StockExport(models.TransientModel):
         picking_type_out = warehouse.out_type_id
         picking_types = picking_type_in | picking_type_out
 
-        moves = Move.search([('state', '=', 'done'),
-                             ('picking_type_id', 'in', picking_types.ids),
-                             ('date', '>', self.date + " 00:00:00"),
-                             ('date', '<', self.date + " 23:59:59")])
+        moves = Move.search(
+            [
+                ("state", "=", "done"),
+                ("picking_type_id", "in", picking_types.ids),
+                ("date", ">", self.date + " 00:00:00"),
+                ("date", "<", self.date + " 23:59:59"),
+            ]
+        )
 
         in_moves = moves.filtered(lambda m: m.picking_type_id == picking_type_in)
         out_moves = moves.filtered(lambda m: m.picking_type_id == picking_type_out)
@@ -182,18 +196,20 @@ class StockExport(models.TransientModel):
         for goods_moves, sheet_name in [(in_moves, "Goods In"), (out_moves, "Goods Out")]:
             move_data = {
                 "name": sheet_name,
-                "column_titles": ['Reference', 'Part number', 'Package', 'Quantity']
+                "column_titles": ["Reference", "Part number", "Package", "Quantity"],
             }
             rows = []
             for move in goods_moves:
-                for move_line in move.mapped('move_line_ids'):
+                for move_line in move.mapped("move_line_ids"):
                     rows.append(
-                        {'Reference': move.picking_id.origin,
-                         'Part number': move.product_id.display_name,
-                         'Package': move_line.result_package_id.name,
-                         'Quantity': move_line.qty_done}
+                        {
+                            "Reference": move.picking_id.origin,
+                            "Part number": move.product_id.display_name,
+                            "Package": move_line.result_package_id.name,
+                            "Quantity": move_line.qty_done,
+                        }
                     )
-            move_data['rows'] = rows
+            move_data["rows"] = rows
             data.append(move_data)
 
         return data
@@ -208,7 +224,7 @@ class StockExport(models.TransientModel):
         that contains data for the rows [{r1}, {r2}, ...].
         """
         # Generate workbook from data
-        _logger.info(_('Creating Excel file'))
+        _logger.info(_("Creating Excel file"))
         wb = xlwt.Workbook()
         for sheet_data in sheets_data:
             column_names = sheet_data.get("column_titles")
@@ -225,25 +241,23 @@ class StockExport(models.TransientModel):
         self.file_name = file_name
         self.file_data = file_data
 
-        attachment = self.env['ir.attachment'].create(
-            {'name': file_name,
-             'type': 'binary',
-             'datas': file_data,
-             'datas_fname': file_name})
+        attachment = self.env["ir.attachment"].create(
+            {"name": file_name, "type": "binary", "datas": file_data, "datas_fname": file_name}
+        )
 
         # Send stock file via requested method
-        if send_file_via == 'email':
+        if send_file_via == "email":
             self._send_email(attachment)
-        elif send_file_via == 'user':
+        elif send_file_via == "user":
             self._send_message_to_user(file_name, doc_title, attachment)
         else:
             _logger.warning(
-                _('Stock file was created but '
-                  'not sent (send_file_via: %s') % send_file_via)
+                _("Stock file was created but " "not sent (send_file_via: %s") % send_file_via
+            )
 
     def _send_message_to_user(self, doc_title, file_name, attachment):
-        '''Send attachement to the user via an intenal message'''
-        Users = self.env['res.users']
+        """Send attachement to the user via an intenal message"""
+        Users = self.env["res.users"]
         Users.send_message_to_user(
             subject="%s Ready" % doc_title,
             body=_("%s %s is attached.") % (doc_title, file_name),
@@ -251,19 +265,17 @@ class StockExport(models.TransientModel):
         )
 
     def _send_email(self, attachment, email_template=None):
-        '''
+        """
         Send attachement via email, if email_template is None,
         the automated_stock_email_template is used.
-        '''
-        Mail = self.env['mail.mail']
+        """
+        Mail = self.env["mail.mail"]
 
         # To allow reuse!
         if email_template is None:
-            email_template = self.env.ref(
-                'udes_report.automated_stock_email_template'
-            )
+            email_template = self.env.ref("udes_report.automated_stock_email_template")
         # Attachment file
-        email_template.write({'attachment_ids': [(6, 0, [attachment.id])]})
+        email_template.write({"attachment_ids": [(6, 0, [attachment.id])]})
 
         # Makes email to send
         mail_id = email_template.send_mail(self.env.uid)
@@ -273,19 +285,20 @@ class StockExport(models.TransientModel):
 
         # As it auto deletes then exists should be empty
         # but incase this changes I'll check state as well
-        if not mail.exists() or mail.state == 'sent':
-            _logger.info(_('Stock email sent'))
+        if not mail.exists() or mail.state == "sent":
+            _logger.info(_("Stock email sent"))
         # if sending fails odoo will raise its own error
 
         # Reset email_template attachment
-        email_template.write({'attachment_ids': [(6, 0, [])]})
+        email_template.write({"attachment_ids": [(6, 0, [])]})
 
     @api.model
     def send_automated_stock_file(self):
-        ''' Sends an email with the stock file attached'''
-        Export = self.env['udes_report.stock_export']
+        """ Sends an email with the stock file attached"""
+        Export = self.env["udes_report.stock_export"]
         export = Export.create([])
-        export.run_stock_file_export(send_file_via='email')
+        export.run_stock_file_export(send_file_via="email")
+
 
 #
 # Helpers
