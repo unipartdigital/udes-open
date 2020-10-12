a3162e883c110e7308b1389cdff4defb57a7c132
Robert Smith <robert.smith@unipart.io>
2020-10-12 13:32:31 +0100

    [ADD] udes_warehouse_classification: Add helper function to get messages for report
    Using report reference, find the specific messages required for a given product.
    
    User-story: 11592

--- addons/udes_warehouse_classification/models/product_template.py
+++ addons/udes_warehouse_classification/models/product_template.py
@@ -12,6 +12,11 @@ class ProductTemplate(models.Model):
         help="Classifications on products used for messaging purposes.",
     )
 
-    # def _get_classification_reports(self, picking):
-    #     """Find product classifications needed for a given report"""
-    #     return self.mapped("u_product_warehouse_classification_ids.report_template_ids").
+    def get_classification_messages_for_report(self, report_name):
+        """Find product classification messages needed for a given report"""
+        report = self.env.ref(report_name)
+        return (
+            self.mapped("u_product_warehouse_classification_ids")
+            .filtered(lambda c: report in c.report_template_ids)
+            .mapped("report_message")
+        )
