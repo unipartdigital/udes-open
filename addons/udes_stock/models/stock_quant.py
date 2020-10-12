04770a01d1fe5b5b5f3aa63bc89731f805660575
Peter Pratt <peter.pratt@unipart.io>
2020-10-12 08:30:32 +0100

    [FMT] udes_stock: Format stock_quant.py with black
    User-story: 11534
    
    Signed-off-by: Pete Pratt <peter.pratt@unipart.io>

--- addons/udes_stock/models/stock_quant.py
+++ addons/udes_stock/models/stock_quant.py
@@ -5,8 +5,9 @@ from odoo.exceptions import ValidationError
 
 from collections import defaultdict
 
+
 class StockQuant(models.Model):
-    _inherit = 'stock.quant'
+    _inherit = "stock.quant"
 
     @api.model
     def _get_removal_strategy_order(self, removal_strategy=None):
@@ -15,46 +16,52 @@ class StockQuant(models.Model):
         if removal_strategy == "fifo":
             return "in_date ASC NULLS FIRST, package_id ASC NULLS FIRST"
         return super(Quant, self)._get_removal_strategy_order(removal_strategy)
-    
+
     def assert_not_reserved(self):
         """Ensure all quants in the recordset are unreserved."""
         reserved = self.filtered(lambda q: q.reserved_quantity > 0)
         if reserved:
-            raise ValidationError(_('Items are reserved and cannot be moved. '
-                                    'Please speak to a team leader to resolve '
-                                    'the issue.\nAffected Items: %s') % (
-                                        ' '.join(reserved.mapped('package_id.name'))
-                                        if reserved.mapped('package_id')
-                                        else ' '.join(reserved.mapped('product_id.display_name'))))
+            raise ValidationError(
+                _(
+                    "Items are reserved and cannot be moved. "
+                    "Please speak to a team leader to resolve "
+                    "the issue.\nAffected Items: %s"
+                )
+                % (
+                    " ".join(reserved.mapped("package_id.name"))
+                    if reserved.mapped("package_id")
+                    else " ".join(reserved.mapped("product_id.display_name"))
+                )
+            )
 
     def assert_entire_packages(self):
         """Ensure the recordset self contains all the quants in package present
         in the recordset."""
-        packages = self.mapped('package_id')
+        packages = self.mapped("package_id")
         package_quant_ids = packages._get_contained_quants()
 
         diff = package_quant_ids - self
         if diff:
-            prob_packs = diff.mapped('package_id')
-            raise ValidationError(_('Not all quants have been taken.\n'
-                                    'Incomplete Packages:\n'
-                                    '%s') % (' '.join(prob_packs.mapped('name'))))
+            prob_packs = diff.mapped("package_id")
+            raise ValidationError(
+                _("Not all quants have been taken.\n" "Incomplete Packages:\n" "%s")
+                % (" ".join(prob_packs.mapped("name")))
+            )
 
     def assert_valid_location(self, location_id):
         """ Ensure the recorset self contains quants child of location_id
         """
         # TODO: check this function again, create generic is_valid/are_valid?
-        Location = self.env['stock.location']
-        n_quant_locs = len(self.mapped('location_id'))
-        child_locs = Location.search([
-                ('id', 'child_of', location_id),
-                ('id', 'in', self.mapped('location_id.id'))
-            ])
+        Location = self.env["stock.location"]
+        n_quant_locs = len(self.mapped("location_id"))
+        child_locs = Location.search(
+            [("id", "child_of", location_id), ("id", "in", self.mapped("location_id.id"))]
+        )
         if len(child_locs) != n_quant_locs:
             raise ValidationError(
-                    _('The locations of some quants are not children of'
-                      ' location %s') %
-                        Location.browse(location_id).name)
+                _("The locations of some quants are not children of" " location %s")
+                % Location.browse(location_id).name
+            )
 
     def _gather(self, product_id, location_id, **kwargs):
         """ Call default _gather function, if quant_ids context variable
@@ -63,7 +70,7 @@ class StockQuant(models.Model):
             Context variable quant_ids might contain quants of different products.
         """
         quants = super(StockQuant, self)._gather(product_id, location_id, **kwargs)
-        quant_ids = self.env.context.get('quant_ids')
+        quant_ids = self.env.context.get("quant_ids")
         if quant_ids:
             quants = quants.filtered(lambda q: q.id in quant_ids)
         return quants
@@ -71,7 +78,7 @@ class StockQuant(models.Model):
     def total_quantity(self):
         """ Returns the total quantity of the quants in self
         """
-        return sum(self.mapped('quantity'))
+        return sum(self.mapped("quantity"))
 
     def group_quantity_by_product(self, only_available=False):
         """ Returns a dictionary with the total quantity per product,
@@ -102,16 +109,17 @@ class StockQuant(models.Model):
         if self.package_id:
             package_info = self.package_id.get_info()[0]
 
-        res = {"id": self.id,
-               "package_id": package_info,
-               "product_id": self.product_id.get_info()[0],
-               "location_id": location_info[0],
-               "quantity": self.quantity,
-               "reserved_quantity": self.reserved_quantity,
-               }
+        res = {
+            "id": self.id,
+            "package_id": package_info,
+            "product_id": self.product_id.get_info()[0],
+            "location_id": location_info[0],
+            "quantity": self.quantity,
+            "reserved_quantity": self.reserved_quantity,
+        }
 
         if self.lot_id:
-            res['lot_id'] = self.lot_id.get_info()[0]
+            res["lot_id"] = self.lot_id.get_info()[0]
 
         return res
 
@@ -124,7 +132,7 @@ class StockQuant(models.Model):
 
         return res
 
-    @api.constrains('location_id')
+    @api.constrains("location_id")
     def quant_location_policy(self):
         self.ensure_one()
         self.location_id.apply_quant_policy()
