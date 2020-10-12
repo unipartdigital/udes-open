9f8a4014e6102812a75a337d0fde8a2cdfcab981
Robert Smith <robert.smith@unipart.io>
2020-10-12 13:32:49 +0100

    [ADD] udes_warehouse_classification: Extend _prepare_info
    Extending _prepare_info to include classification messages for a
    given picking
    
    User-story: 11592
    Signed-off-by: Robert Smith <robert.smith@unipart.io>

--- addons/udes_warehouse_classification/models/stock_picking.py
+++ addons/udes_warehouse_classification/models/stock_picking.py
@@ -6,8 +6,29 @@ from odoo import fields, models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def _get_product_classfication_messages(self):
-        """Find product classifications needed for a given picking"""
-        return self.mapped("move_lines.product_id.u_product_warehouse_classification_ids").filtered(
-            lambda c: self.picking_type_id in c.picking_type_ids
+    def _get_classification_messages_for_product_picking(self):
+        """Return the product with classification messages appropriate for a picking"""
+        products = self.mapped("move_lines.product_id")
+        product_classifications = {}
+        for product in products:
+            classifications = product.u_product_warehouse_classification_ids.filtered(
+                lambda c: self.picking_type_id in c.picking_type_ids
+            ).sorted("sequence")
+            product_classifications[product.barcode] = [
+                {"message": classification.alert_message} for classification in classifications
+            ]
+        return product_classifications
+
+    def _prepare_info(self, priorities=None, fields_to_fetch=None):
+        """{doc}
+           Extensions:
+           - u_classification_messages: list
+        """.format(
+            doc=super()._prepare_info.__doc__
         )
+        info = super()._prepare_info(priorities=priorities, fields_to_fetch=fields_to_fetch)
+        if fields_to_fetch is None or "u_classification_messages" in fields_to_fetch:
+            info[
+                "u_classification_messages"
+            ] = self._get_classification_messages_for_product_picking()
+        return info
