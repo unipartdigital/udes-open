8a79eff4a824c872e7b354e4c7a3d74b2657fa79
Robert Smith <robert.smith@unipart.io>
2020-10-16 12:05:41 +0100

    [ADD] udes_warehouse_classification: Add endpoint for unexpected product
    When a product is previously unexpected on a picking we need to be able to
    check for messages directly for the product as we can't know before hand.
    
    User-story: 11593
    Signed-off-by: Robert Smith <robert.smith@unipart.io>

---
+++ addons/udes_warehouse_classification/controllers/product_template.py
@@ -0,0 +1,35 @@
+from odoo import http, _
+from odoo.http import request
+
+
+class ControllerProductClassifications(http.Controller):
+
+    @http.route('/api/product-product/get_product_classifications/', type='json', methods=['GET'], auth='user')
+    def get_classification_messages_for_product_picking(
+        self, product_barcode, picking_type_id=None
+    ):
+        """ Search for product classification messages by product barcode and picking type
+            returns the messages which match the given criteria.
+
+            @param product_barcode
+                This is a string that entirely matches the barcode
+            @param picking_type_id (optional)
+                This is the string for the current picking type
+
+        """
+        Product = request.env["product.product"]
+        product = Product.get_product(product_barcode)
+        product_classifications = product.u_product_warehouse_classification_ids
+
+        if not picking_type_id:
+            classifications = product_classifications
+
+        else:
+            classifications = product_classifications.filtered(
+                lambda c: picking_type_id in c.picking_type_ids.mapped('id')
+            )
+
+        return [
+            {"message": message}
+            for message in classifications.sorted("sequence").mapped("alert_message")
+        ]
