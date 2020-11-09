7f931fe2d12367816e8fc89f18ebae3a4361ea3f
Peter Clark <peter.clark@unipart.io>
2020-11-09 16:01:23 +0000

    [ADD] udes_stock: API endpoint added for Package Type
    Added API endpoint to retrieve all active Package Types along with _prepare_info and get_info methods to send over the relevant information.
    
    Story/11788
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_stock/controllers/__init__.py
+++ addons/udes_stock/controllers/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import main
+from . import product_packaging
 from . import product_product
 from . import stock_location
 from . import stock_location_category
--- addons/udes_stock/models/product_packaging.py
+++ addons/udes_stock/models/product_packaging.py
@@ -7,3 +7,37 @@ class ProductPackaging(models.Model):
     _inherit = "product.packaging"
 
     active = fields.Boolean("Active", default=True)
+
+    def _prepare_info(self, fields_to_fetch=None):
+        """
+            Prepares the following info of the product in self:
+            - id: int
+            - name: string
+            - sequence: int
+
+            @param (optional) fields_to_fetch
+                Subset of the default fields to return
+        """
+        self.ensure_one()
+
+        packaging_info = {
+            "id": lambda p: p.id,
+            "name": lambda p: p.name,
+            "sequence": lambda p: p.sequence,
+        }
+
+        if not fields_to_fetch:
+            fields_to_fetch = packaging_info.keys()
+
+        return {key: value(self) for key, value in packaging_info.items() if key in fields_to_fetch}
+
+    def get_info(self, **kwargs):
+        """
+        Return a list with the information of each packaging record in self.
+        """
+        res = []
+
+        for packaging in self:
+            res.append(packaging._prepare_info(**kwargs))
+
+        return res
