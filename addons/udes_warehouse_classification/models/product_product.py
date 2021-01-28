536794a3cc94f2cbcf6e98137b3a237d0a8b9f1a
Anthony Williams <anthony.williams@unipart.io>
2021-01-28 11:09:09 +0000

    [ADD] udes_warehouse_classification: Add product classification string
    User-story: 10344
    Signed-off-by: Anthony Williams <anthony.williams@unipart.io>

--- addons/udes_warehouse_classification/models/product_product.py
+++ addons/udes_warehouse_classification/models/product_product.py
@@ -7,3 +7,12 @@ class ProductProduct(models.Model):
     def get_classification_messages_for_report(self, report_name):
         """Find product classification messages needed for a given report"""
         return self.mapped("product_tmpl_id").get_classification_messages_for_report(report_name)
+
+    def get_printable_classification_names(self, separator="", order_key="name"):
+        """Return a string of the product classifications in self"""
+        return separator.join(
+            self
+            .mapped("u_product_warehouse_classification_ids")
+            .sorted(order_key)
+            .mapped("name")
+        )
