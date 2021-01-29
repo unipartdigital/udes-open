4f1d1d274f465df886c3f16f0883987d7fdc1e11
Sean Quah <sean.quah@unipart.io>
2021-01-29 11:24:04 +0000

    [FMT] udes_stock: Run formatter on product_product.py
    Signed-off-by: Sean Quah <sean.quah@unipart.io>

--- addons/udes_stock/models/product_product.py
+++ addons/udes_stock/models/product_product.py
@@ -3,30 +3,28 @@
 from odoo import fields, models, _
 from odoo.exceptions import ValidationError
 
-BASE_PRODUCT_IMAGE_URL = '/web/image/product.product/%i'
+BASE_PRODUCT_IMAGE_URL = "/web/image/product.product/%i"
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     # Add tracking for archiving.
-    active = fields.Boolean(track_visibility='onchange')
+    active = fields.Boolean(track_visibility="onchange")
 
     def assert_serial_numbers(self, serial_numbers):
         """
         If the product in self is tracked by serial numbers, check if
         the ones in serial_numbers are currently in use in the system.
         """
-        Lot = self.env['stock.production.lot']
+        Lot = self.env["stock.production.lot"]
         self.ensure_one()
-        if self.tracking == 'serial':
-            lots = Lot.search([('product_id', '=', self.id),
-                               ('name', 'in', serial_numbers)
-                               ])
+        if self.tracking == "serial":
+            lots = Lot.search([("product_id", "=", self.id), ("name", "in", serial_numbers)])
             if lots:
                 raise ValidationError(
-                    _('Serial numbers %s already in use for product %s') %
-                    (' '.join(serial_numbers), self.name)
+                    _("Serial numbers %s already in use for product %s")
+                    % (" ".join(serial_numbers), self.name)
                 )
 
     def _prepare_info(self, fields_to_fetch=None):
@@ -47,26 +45,26 @@ class ProductProduct(models.Model):
             if p.image:
                 base_url = BASE_PRODUCT_IMAGE_URL % p.id
                 image_urls = {
-                    'large': base_url + '/image',
-                    'medium': base_url + '/image_medium',
-                    'small': base_url + '/image_small',
+                    "large": base_url + "/image",
+                    "medium": base_url + "/image_medium",
+                    "small": base_url + "/image_small",
                 }
                 return image_urls
             return {}
 
-        info = {"id": lambda p: p.id,
-                "barcode": lambda p: p.barcode,
-                "display_name": lambda p: p.display_name,
-                "name": lambda p: p.display_name,
-                "tracking": lambda p: p.tracking,
-                "image_urls": _prepare_image_urls,
-               }
+        info = {
+            "id": lambda p: p.id,
+            "barcode": lambda p: p.barcode,
+            "display_name": lambda p: p.display_name,
+            "name": lambda p: p.display_name,
+            "tracking": lambda p: p.tracking,
+            "image_urls": _prepare_image_urls,
+        }
 
         if not fields_to_fetch:
             fields_to_fetch = info.keys()
 
         return {key: value(self) for key, value in info.items() if key in fields_to_fetch}
-
 
     def get_info(self, **kwargs):
         """ Return a list with the information of each product in self.
@@ -81,17 +79,21 @@ class ProductProduct(models.Model):
         """ Get product from a name, barcode, or id.
         """
         if isinstance(product_identifier, int):
-            domain = [('id', '=', product_identifier)]
+            domain = [("id", "=", product_identifier)]
         elif isinstance(product_identifier, str):
-            domain = ['|', ('barcode', '=', product_identifier),
-                           ('name', '=', product_identifier)]
+            domain = ["|", ("barcode", "=", product_identifier), ("name", "=", product_identifier)]
         else:
-            raise ValidationError(_('Unable to create domain for product search from identifier of type %s') % type(product_identifier))
+            raise ValidationError(
+                _("Unable to create domain for product search from identifier of type %s")
+                % type(product_identifier)
+            )
 
         results = self.search(domain)
         if not results:
-            raise ValidationError(_('Invalid product scanned: %s') % str(product_identifier))
-        if  len(results) > 1:
-            raise ValidationError(_('Too many products found for identifier %s') % str(product_identifier))
+            raise ValidationError(_("Invalid product scanned: %s") % str(product_identifier))
+        if len(results) > 1:
+            raise ValidationError(
+                _("Too many products found for identifier %s") % str(product_identifier)
+            )
 
         return results
