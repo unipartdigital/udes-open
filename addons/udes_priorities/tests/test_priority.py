5f48c57fbc4068c7380b2b0085b8dc3c59a73360
Lorenzo Cucurachi <lorenzo.cucurachi@unipart.io>
2021-10-29 16:14:11 +0100

    [REF] udes_priorities: change priorities user restrictions
    User-story: 20
    
    A Priority can now be deleted or archived only by admins or trusted
    users if there are no transfers with this priority assigned to them.
    
    A stock transfers count is now added to Priorities showing how many
    transfers are using the priority. This is helpful to understand how
    changes could affect stock tranfers.
    
    Transfer types can now be added to a priority at any time and deleted
    only if there are no transfers of the same type using the priority.
    
    Reference field now cannot be changed by any user, including trusted
    or admin.
    
    Picking types from a priority can now be removed only if there are
    no stock transfers assigned to the priority. If the user tries to
    remove a picking type that has transfers assigned then a message will
    popup when trying to save. Originally a similar method was implemented
    on picking_type_ids with an api.onchange decorator but Odoo was
    creating further issues as a spur priority record was being created
    and saved in the database.
    
    Unit Tests have been added to confirm the changes are effective.
    
    Signed-off-by: Lorenzo Cucurachi <lorenzo.cucurachi@unipart.io>

--- addons/udes_priorities/tests/test_priority.py
+++ addons/udes_priorities/tests/test_priority.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from . import common
-from odoo.exceptions import UserError
+from odoo.exceptions import AccessError, UserError, ValidationError
 
 
 class TestPriorities(common.BasePriorities):
@@ -28,21 +27,65 @@ class TestPriorities(common.BasePriorities):
 
         actual_order_2 = Priorities.search([("id", "in", self.test_priorities.ids)])
         for retrieved, ordered in zip(actual_order_2, new_order):
-            self.assertEqual(retrieved, ordered, "Incorrect ordering after being modified")
+            self.assertEqual(
+                retrieved, ordered, "Incorrect ordering after being modified"
+            )
 
-    def test_outstanding_picking(self):
-        self.urgent.reference = "This is allowed"
+    def test_check_other_user_rights_for_priorities(self):
+        """
+        Check that non trusted or non admin users cannot modify priorities.
+        """
+        with self.assertRaises(UserError):
+            self.urgent.sudo(self.simple_stock_usr).reference = "This is not allowed"
 
-        picking = self.create_picking(self.picking_type_pick)
-        picking.priority = self.urgent.reference
-        with self.assertRaises(
-            UserError, msg="Allowed to change reference while there is outstanding an picking"
-        ):
+        with self.assertRaises(AccessError):
+            self.urgent.sudo(self.simple_stock_usr).description = "This is not allowed"
+
+        with self.assertRaises(UserError):
+            self.urgent.sudo(self.simple_stock_usr).active = False
+
+        with self.assertRaises(UserError):
+            self.urgent.sudo(self.simple_stock_usr).unlink()
+
+    def test_check_editable_fields_for_priorities(self):
+        """
+        Check that the correct users can modify specific fields.
+        """
+        self.urgent.name = "This is allowed1"
+        self.urgent.sudo(self.trusted_usr).name = "This is allowed2"
+
+        self.urgent.description = "This is allowed1"
+        self.urgent.sudo(self.trusted_usr).description = "This is allowed2"
+
+        with self.assertRaises(UserError):
             self.urgent.reference = "This is not allowed"
+        with self.assertRaises(UserError):
+            self.urgent.sudo(self.trusted_usr).reference = "This is not allowed"
 
-        self.urgent.description = "This is still allowed"
+        self.urgent.sudo(self.trusted_usr).sequence = "3"
 
-        with self.assertRaises(
-            UserError, msg="Allowed to unlink while there is outstanding an picking"
-        ):
+        self.urgent.sudo(self.trusted_usr).active = False
+        self.urgent.sudo(self.trusted_usr).active = True
+
+        self.urgent.sudo(self.trusted_usr).picking_type_ids += self.picking_type_pick
+
+    def test_cannot_remove_picking_type_with_assigned_transfers(self):
+        picking1 = self.create_picking(self.picking_type_pick)
+        picking1.priority = self.urgent.reference
+
+        with self.assertRaises(UserError):
+            self.urgent.picking_type_ids -= self.picking_type_pick
+
+    def test_cannot_archive_picking_type_with_assigned_transfers(self):
+        picking1 = self.create_picking(self.picking_type_pick)
+        picking1.priority = self.urgent.reference
+
+        with self.assertRaises(ValidationError):
+            self.urgent.active = False
+
+    def test_cannot_remove_priority_with_assigned_transfers(self):
+        picking1 = self.create_picking(self.picking_type_pick)
+        picking1.priority = self.urgent.reference
+
+        with self.assertRaises(UserError):
             self.urgent.unlink()
