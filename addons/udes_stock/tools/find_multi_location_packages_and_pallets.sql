2785b953dbc761754d6b278b2140c1d57bf5fb9e
Adam Patrick <adam.patrick@unipart.io>
2021-11-11 14:22:54 +0000

    [IMP] udes-open: Update to multi-location package script
    Added package, pallet and location names to the queries.
    
    Story/x12
    
    Signed-off-by: Adam Patrick <adam.patrick@unipart.io>

--- addons/udes_stock/tools/find_multi_location_packages_and_pallets.sql
+++ addons/udes_stock/tools/find_multi_location_packages_and_pallets.sql
@@ -1,46 +1,72 @@
 \timing
 
 -- Find packages/pallets of products in multiple locations
+\echo ''
+\echo 'Finding packages in multiple locations:'
 SELECT
     p.package_id,
-    COUNT(location_id)
+    p.package_name,
+    COUNT(location_id) AS location_count,
+    string_agg(CAST(location_id AS VARCHAR(10)), ', ') AS location_ids,
+    string_agg(CAST(location_name AS VARCHAR(100)), ', ') AS location_names
 FROM (
     SELECT
-        package_id,
-        location_id
+        q.package_id,
+        q.location_id,
+        loc.name AS location_name,
+        pack.name AS package_name
     FROM
-        stock_quant
+        stock_quant q
+    INNER JOIN 
+    	stock_quant_package pack ON pack.id = q.package_id 
+    INNER JOIN 
+    	stock_location loc ON loc.id = q.location_id
     WHERE
-        package_id IS NOT NULL
+        q.package_id IS NOT NULL
     GROUP BY
-        package_id,
-        location_id
+        q.package_id,
+        q.location_id,
+        pack.name,
+        loc.name
 ) AS p
 GROUP BY
-    p.package_id
+    p.package_id,
+    p.package_name
 HAVING
     COUNT(location_id) > 1
 ;
 
 -- Find pallets of packages in mutliple locations
+\echo ''
+\echo 'Finding pallets in multiple locations:'
 SELECT
     p.pallet_id,
-    COUNT(location_id)
+    p.pallet_name,
+    COUNT(location_id) AS location_count,
+    string_agg(CAST(location_id AS VARCHAR(10)), ', ') AS location_ids,
+    string_agg(CAST(location_name AS VARCHAR(100)), ', ') AS location_names
 FROM (
     SELECT
         pallet.id AS pallet_id,
-        q.location_id
+        pallet.name AS pallet_name,
+        q.location_id,
+        loc.name AS location_name
     FROM stock_quant q
     INNER JOIN
         stock_quant_package pack ON pack.id = q.package_id
     INNER JOIN
         stock_quant_package pallet ON pallet.id = pack.package_id
+    INNER JOIN
+    	stock_location loc ON loc.id = q.location_id
     GROUP BY
         pallet.id,
-        q.location_id
+        pallet.name,
+        q.location_id,
+        loc.name
 ) AS p
 GROUP BY
-    p.pallet_id
+    p.pallet_id,
+    p.pallet_name
 HAVING
     COUNT(location_id) > 1
 ;
