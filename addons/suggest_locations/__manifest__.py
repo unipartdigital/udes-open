4ea0a65f2847ae7f4d73bcbdd47aa6c3b529a443
Peter Alabaster <peter.alabaster@unipart.io>
2021-12-02 17:08:25 +0000

    [ADD] suggest_locations, get_info modules from 13.0 branch
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.io>
    
    User-story: 53

---
+++ addons/suggest_locations/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "suggest_locations",
+    "summary": "Suggested locations",
+    "description": "Suggested locations",
+    "author": "Unipart digital",
+    "website": "http://github/unipartdigital/udes-open",
+    "category": "UDES",
+    "version": "0.1",
+    "depends": ["base", "get_info", "udes_common", "udes_stock"],
+    "data": ["views/stock_picking_type.xml"],
+}
