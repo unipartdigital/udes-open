57e36a026d4e1c0b90922461ef72c4643f7775c8
Peter Clark <peter.clark@unipart.io>
2021-12-13 23:40:42 +0000

    [REM] udes_stock: Removed test numbers from tests in TestStockPicking
    Story/53
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_stock/tests/test_stock_picking.py
+++ addons/udes_stock/tests/test_stock_picking.py
@@ -65,7 +65,7 @@ class TestStockPicking(common.BaseUDES):
                         f"'{field}' for {picking.name} should be '{expected_pick_names}'",
                     )
 
-    def test01_get_empty_locations(self):
+    def test_get_empty_locations(self):
         """Get empty locations - for goods in"""
         self.assertEqual(
             self.test_picking_in.get_empty_locations(),
@@ -79,12 +79,12 @@ class TestStockPicking(common.BaseUDES):
         )
         self.assertFalse(self.test_picking_in.get_empty_locations())
 
-    def test02_get_child_locations_simple_success(self):
+    def test_get_child_locations_simple_success(self):
         """Get child locations"""
         locations = self.test_goodsout_locations | self.out_location
         self.assertEqual(self.test_picking_pick._get_child_dest_locations(), locations)
 
-    def test03_get_child_locations_simple_success_with_extra_domain(self):
+    def test_get_child_locations_simple_success_with_extra_domain(self):
         """Get child locations - with extra domain"""
         aux_domain = [("name", "=", self.test_goodsout_location_01.name)]
         self.assertEqual(
@@ -92,12 +92,12 @@ class TestStockPicking(common.BaseUDES):
             self.test_goodsout_location_01,
         )
 
-    def test04_get_child_locations_with_incorrrect_extra_domain(self):
+    def test_get_child_locations_with_incorrrect_extra_domain(self):
         """Return no child locations when an incorrect extra domain is given, no error is thrown"""
         aux_domain = [("name", "=", "Not a location")]
         self.assertFalse(self.test_picking_pick._get_child_dest_locations(aux_domain=aux_domain))
 
-    def test05_create_picking_no_moves(self):
+    def test_create_picking_no_moves(self):
         """Create a picking from picking type but has no moves created as no products_info given"""
         pick = self.Picking.create_picking(picking_type=self.picking_type_pick)
         # Check pick created with correct picking type
@@ -109,7 +109,7 @@ class TestStockPicking(common.BaseUDES):
         # Check the number of moves is zero
         self.assertEqual(len(pick.move_lines), 0)
 
-    def test06_create_picking_success_simple(self):
+    def test_create_picking_success_simple(self):
         """Create a picking from picking type with two products in state draft"""
         self.create_quant(self.apple.id, self.test_stock_location_01.id, 50)
         self.create_quant(self.banana.id, self.test_stock_location_01.id, 50)
@@ -131,7 +131,7 @@ class TestStockPicking(common.BaseUDES):
         # Check batch not created
         self.assertFalse(pick.batch_id)
 
-    def test07_create_picking_success_custom_locations(self):
+    def test_create_picking_success_custom_locations(self):
         """Create a picking with non-default locations and confirm"""
         products = self.apple | self.banana
         pick = self.Picking.create_picking(
@@ -159,7 +159,7 @@ class TestStockPicking(common.BaseUDES):
         # Check batch created
         self.assertTrue(pick.batch_id)
 
-    def test08_create_multiple_pickings(self):
+    def test_create_multiple_pickings(self):
         """Create multiple pickings with non-default locations and priority"""
         products = self.apple | self.banana
         picks = self.Picking.create_picking(
@@ -194,7 +194,7 @@ class TestStockPicking(common.BaseUDES):
         # Check batch created
         self.assertTrue(picks.batch_id)
 
-    def test09_pepare_and_create_move(self):
+    def test_pepare_and_create_move(self):
         """Prepare and create a single move"""
         pick = self.create_picking(self.picking_type_goods_in)
         move_values = self.Picking._prepare_move(
@@ -211,7 +211,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertEqual(len(new_move), 1)
         self.assertEqual(new_move.product_qty, 10)
 
-    def test10_prepare_and_create_multiple_moves(self):
+    def test_prepare_and_create_multiple_moves(self):
         """Prepare and create multiple moves"""
         products_info = [[{"product": self.apple, "qty": 10}], [{"product": self.fig, "qty": 10}]]
         pick1 = self.create_picking(self.picking_type_goods_in)
@@ -231,7 +231,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertEqual(len(new_move), 2)
         self.assertEqual(new_move.product_id, (self.fig | self.apple))
 
-    def test11_get_move_lines(self):
+    def test_get_move_lines(self):
         """Test three cases of get_move_lines: when done=None, True and False"""
         # Get all moves and move lines associated to the picking
         move_lines = self.test_picking_in.get_move_lines()
@@ -249,7 +249,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertFalse(self.test_picking_in.get_move_lines(done=False))
         self.assertEqual(move_lines.qty_done, 10)
 
-    def test12_requires_backorder_simple(self):
+    def test_requires_backorder_simple(self):
         """Simple requires backorder check"""
         # Update moves
         self.test_picking_in.move_lines.quantity_done = 10
@@ -257,7 +257,7 @@ class TestStockPicking(common.BaseUDES):
         mls = self.test_picking_in.move_line_ids
         self.assertFalse(self.test_picking_in._requires_backorder(mls))
 
-    def test13_requires_backorder_multi_lines(self):
+    def test_requires_backorder_multi_lines(self):
         """Create picking with multiple lines backorder check"""
         self.create_quant(self.fig.id, self.test_stock_location_02.id, 50)
         self.create_quant(self.banana.id, self.test_stock_location_02.id, 50)
@@ -272,7 +272,7 @@ class TestStockPicking(common.BaseUDES):
         mls = pick.move_line_ids
         self.assertTrue(pick._requires_backorder(mls))
 
-    def test14_backorder_move_lines_default_mls(self):
+    def test_backorder_move_lines_default_mls(self):
         """Backorder move lines for incomplete pick"""
         self.create_quant(self.fig.id, self.test_stock_location_02.id, 50)
         self.create_quant(self.banana.id, self.test_stock_location_02.id, 50)
@@ -290,7 +290,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertEqual(bk_picking.move_lines.mapped("product_qty"), [50.0, 50.0])
         self.assertEqual(bk_picking.move_line_ids.mapped("qty_done"), [10.0, 10.0])
 
-    def test15_backorder_move_lines_fig_mls(self):
+    def test_backorder_move_lines_fig_mls(self):
         """Backorder move lines for incomplete pick, but only for a subset of mls"""
         self.create_quant(self.fig.id, self.test_stock_location_02.id, 50)
         self.create_quant(self.banana.id, self.test_stock_location_02.id, 50)
@@ -314,7 +314,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertEqual(pick.move_lines.product_qty, 50)
         self.assertEqual(pick.move_line_ids.qty_done, 5)
 
-    def test16_backorder_move_lines_error(self):
+    def test_backorder_move_lines_error(self):
         """Backorder move lines raises error when nothing is done"""
         self.create_quant(self.fig.id, self.test_stock_location_02.id, 50)
         self.create_quant(self.banana.id, self.test_stock_location_02.id, 50)
@@ -328,7 +328,7 @@ class TestStockPicking(common.BaseUDES):
         msg = "There are no move lines within picking %s to backorder" % pick.name
         self.assertEqual(e.exception.args[0], msg)
 
-    def test17_backorder_move_lines_all_qts_done(self):
+    def test_backorder_move_lines_all_qts_done(self):
         """Create a backorder for a picking when all quantities are done
         Old pick is empty, everything moved to backorder pick
         """
@@ -352,7 +352,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertEqual(len(pick.move_line_ids), 0)
         self.assertEqual(len(pick.move_lines), 0)
 
-    def test18_backorder_move_lines_fulfilled_move_not(self):
+    def test_backorder_move_lines_fulfilled_move_not(self):
         """Check when move lines are fulfilled, but the move is partially fulfilled"""
         self.create_quant(self.fig.id, self.test_stock_location_02.id, 8)
         self.create_quant(self.banana.id, self.test_stock_location_02.id, 10)
@@ -378,7 +378,7 @@ class TestStockPicking(common.BaseUDES):
         self.assertEqual(len(pick.move_line_ids), 0)
         self.assertEqual(pick.move_lines.quantity_done, 0)
 
-    def test19_assert_related_pickings_computed_correctly(self):
+    def test_assert_related_pickings_computed_correctly(self):
         """
         Assert that first/previous/next picking computed fields return expected records
         with the following setup:
@@ -472,7 +472,7 @@ class TestStockPicking(common.BaseUDES):
             all_picks, expected_pickings_by_field
         )
 
-    def test20_assert_created_backorders_computed_correctly(self):
+    def test_assert_created_backorders_computed_correctly(self):
         """Assert that Created Backorders field is computed correctly"""
         apple_qty = 1
         self.create_quant(self.apple.id, self.test_stock_location_01.id, apple_qty)
@@ -508,7 +508,7 @@ class TestStockPicking(common.BaseUDES):
             f"expected backorder: {expected_backorder}",
         )
 
-    def test21_assert_picking_quantities_computed_correctly(self):
+    def test_assert_picking_quantities_computed_correctly(self):
         """Assert that qty todo/done and package discrepancies fields are computed correctly"""
         apple_qty = 10
         apple_qty_per_line = apple_qty / 2
