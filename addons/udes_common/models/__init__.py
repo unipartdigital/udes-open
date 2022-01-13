212469052e2a815c237571c9e9acce86137e8602
Peter Clark <peter.clark@unipart.io>
2022-01-13 16:58:22 +0200

    [ADD] udes_common: Added _is_superuser_or_admin method to ResUsers
    Returns true if user is superuser or admin (admin user created by default by Odoo).
    
    Story/567
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_common/models/__init__.py
+++ addons/udes_common/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import ir_module
+from . import res_users
 from . import models
 from .models import add_if_not_exists
 from . import registry
---
+++ addons/udes_common/models/res_users.py
@@ -0,0 +1,23 @@
+from odoo import models
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    def _is_superuser_or_admin(self):
+        """
+        Return True if user is active user superuser or admin, otherwise False.
+
+        Note: Admin here means the admin user that Odoo creates by default,
+              not just any user with permission to edit settings.
+        """
+        if self.env.user._is_superuser():
+            return True
+
+        try:
+            if self.env.uid == self.env.ref("base.user_admin").id:
+                return True
+        except ValueError:
+            # Admin user has been deleted
+            pass
+
+        return False
