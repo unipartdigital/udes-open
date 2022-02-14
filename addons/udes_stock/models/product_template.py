afdc3d56f2d8a9d3191e041c56500dfb591e2912
Peter Clark <peter.clark@unipart.io>
2022-02-14 23:04:02 +0000

    [IMP] udes_stock: Show inactive product.product records on product.template
    Set "active_test" to False for "product_variant_ids". This means related fields on product.template (like barcode) will be preserved even when the template and product are archived.
    
    Story/955
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_stock/models/product_template.py
+++ addons/udes_stock/models/product_template.py
@@ -56,7 +56,10 @@ class ProductTemplate(models.Model):
         help="Quantity of product per pallet",
     )
 
-    @api.onchange("u_pallet_qty")
+    # Apply context override so that barcode will show even when product is archived
+    product_variant_ids = fields.One2many(context={"active_test": False})
+
+    @api.onchange('u_pallet_qty')
     def disallow_negative_values(self):
         """Prevent values from going below 0."""
         if self.u_pallet_qty < 0:
