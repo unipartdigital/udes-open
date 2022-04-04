381988862ece49f5b2ec36fd142b22ebba331255
Peter Clark <peter.clark@unipart.io>
2022-04-04 23:57:45 +0100

    [IMP] udes_common: Added sudo call to is_module_installed
    Avoids potential permissions error if the active user doesn't have read
    permissions for modules.
    
    Story/1600
    
    Signed-off-by: Peter Clark <peter.clark@unipart.io>

--- addons/udes_common/models/ir_module.py
+++ addons/udes_common/models/ir_module.py
@@ -9,8 +9,12 @@ class Module(models.Model):
         """
         Returns true if the supplied module exists
         and is installed, otherwise false.
+
+        Checks as superuser to avoid potential security restriction.
         """
+        Module = self.env["ir.module.module"].sudo()
+
         search_domain = [("name", "=", module_name), ("state", "=", "installed")]
-        installed_module_count = self.search(search_domain, limit=1, order="id", count=True)
+        installed_module_count = Module.search(search_domain, limit=1, order="id", count=True)
         module_installed = bool(installed_module_count)
         return module_installed
