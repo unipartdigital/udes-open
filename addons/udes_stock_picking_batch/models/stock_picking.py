aaf18b0e5976a8343e9d8be33ba1c7f9b1311141
Armand Cela <armand.cela@unipart.io>
2022-04-12 09:48:33 +0100

    [ADD] udes_stock_picking_batch: Adding is_valid_location_dest_id
    Adding is_valid_location_dest_id on pick and batch
    Story: 1174
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- addons/udes_stock_picking_batch/models/stock_picking.py
+++ addons/udes_stock_picking_batch/models/stock_picking.py
@@ -3,3 +3,34 @@ from odoo import models
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+
+    def get_move_lines_done(self):
+        """ Return the recordset of move lines done. """
+        return self.move_line_ids.filtered(lambda o: o.qty_done > 0)
+
+    def is_valid_location_dest_id(self, location=None, location_ref=None):
+        """Whether the specified location or location reference is a valid
+        putaway location for the picking. Expects a singleton instance.
+
+        Parameters
+        ----------
+        location : Location obj
+            Location record
+        location_ref: char
+            Location identifier can be the ID, name or the barcode
+
+        Returns a boolean indicating the validity check outcome.
+        """
+        Location = self.env["stock.location"]
+        self.ensure_one()
+
+        if not location and not location_ref:
+            raise ValidationError("Must specify a location or ref")
+
+        dest_location = location or Location.get_location(location_ref)
+        if not dest_location:
+            raise ValidationError(_("The specified location is unknown."))
+
+        valid_locations = self._get_child_dest_locations([("id", "=", dest_location.id)])
+
+        return valid_locations.exists()
--- addons/udes_stock_picking_batch/models/stock_picking_batch.py
+++ addons/udes_stock_picking_batch/models/stock_picking_batch.py
@@ -11,3 +11,31 @@ class StockPickingBatch(models.Model):
              "If the batch is in progress, indicates the pallet currently in "
              "use.",
     )
+
+    def is_valid_location_dest_id(self, location_ref):
+        """
+        Whether the specified location is a valid putaway location
+        for the relevant pickings of the batch. Expects a singleton instance.
+
+        Parameters
+        ----------
+        location_ref: char
+            Location identifier can be the ID, name or the barcode
+
+        Returns a boolean indicating the validity check outcome.
+        """
+        self.ensure_one()
+        Location = self.env["stock.location"]
+
+        try:
+            location = Location.get_location(location_ref)
+        except Exception:
+            return False
+
+        done_pickings = self.picking_ids.filtered(lambda p: p.state == "assigned")
+        done_move_lines = done_pickings.get_move_lines_done()
+        all_done_pickings = done_move_lines.picking_id
+
+        return all(
+            [pick.is_valid_location_dest_id(location=location) for pick in all_done_pickings]
+        )
