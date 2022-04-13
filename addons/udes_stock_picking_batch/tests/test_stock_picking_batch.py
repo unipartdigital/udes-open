37f3baf77f844ddd42cb205f5e30e86cdfe77317
Jack <jack.birch@unipart.io>
2022-04-13 15:33:51 +0100

    [ADD] udes_stock_picking_batch: Port create_batch() methods.
    These methods include:
    - create_batch()
    -  _check_user_batch_has_same_picking_types()
    - _check_user_batch_in_progress()
    - _create_batch()
    
    Units tests that included create_batch() functionality were also ported.
    
    Story/1173
    
    Signed off by: Jack Birch <jack.birch@unipart.io>

--- addons/udes_stock_picking_batch/tests/test_stock_picking_batch.py
+++ addons/udes_stock_picking_batch/tests/test_stock_picking_batch.py
@@ -805,3 +805,200 @@ class TestBatchAddRemoveWork(common.BaseUDES):
         batch = batch.assign_user(picking_type_id=self.picking_type_putaway.id)
 
         self.assertEqual(batch.user_id.id, self.env.user.id)
+
+    def test_create_batch_with_priorities(self):
+        """
+        Should create a batch by correctly filtering pickings by
+        priority.
+        """
+        Batch = self.env["stock.picking.batch"]
+        Package = self.env["stock.quant.package"]
+        Batch = Batch.with_user(self.outbound_user)
+
+        for idx in range(2):
+            pack = Package.get_or_create("test_package_%d" % idx, create=True)
+            self.create_quant(
+                self.apple.id, self.test_stock_location_01.id, 4, package_id=pack.id
+            )
+            self.create_picking(
+                self.picking_type_pick,
+                products_info=self.pack_4apples_info,
+                confirm=True,
+                assign=True,
+                priority=str(idx),
+            )
+
+        batch = Batch.create_batch(self.picking_type_pick.id, ["1"])
+
+        self.assertIsNotNone(batch, "No batch created")
+        self.assertEqual(
+            len(batch.picking_ids), 1, "Multiple pickings were included in the batch"
+        )
+        self.assertEqual(
+            batch.picking_ids[0].priority,
+            "2",
+            "Does not have a picking with the expected priority",
+        )
+
+    def test_create_batch_user_already_has_completed_batch(self):
+        """
+        When dropping off a partially reserved picking, a backorder in state
+        confirmed is created and remains in the batch. This backorder should
+        be removed from the batch, allowing the batch to be automatically
+        completed and the user should be able to create a new batch without
+        any problem.
+
+        """
+        Batch = self.env["stock.picking.batch"]
+        Package = self.env["stock.quant.package"]
+        Batch = Batch.with_user(self.outbound_user)
+
+        # set a batch with a complete picking
+        self.create_quant(
+            self.apple.id,
+            self.test_stock_location_01.id,
+            2,
+            package_id=self.package_one.id,
+        )
+        # Create a picking partially reserved
+        picking = self.create_picking(
+            self.picking_type_pick,
+            products_info=self.pack_4apples_info,
+            confirm=True,
+            assign=True,
+        )
+        batch = Batch.create_batch(self.picking_type_pick.id, None)
+        self.assertEqual(batch.picking_ids[0], picking)
+        for ml in picking.move_line_ids:
+            ml.qty_done = ml.product_qty
+        # On drop off a backorder is created for the remaining 2 units,
+        # but _check_batches() removes it from the batch since it is not ready
+        batch.drop_off_picked(
+            continue_batch=True,
+            move_line_ids=None,
+            location_barcode=self.test_output_location_01.name,
+            result_package_name=None,
+        )
+
+        # check the picking is done and the backorder is not in the batch
+        self.assertEqual(len(batch.picking_ids), 1)
+        self.assertEqual(batch.state, "done")
+        self.assertEqual(batch.picking_ids[0].state, "done")
+
+        # create a new picking to be included in the new batch
+        other_pack = Package.get_package("test_other_package", create=True)
+        self.create_quant(
+            self.apple.id, self.test_location_01.id, 4, package_id=other_pack.id
+        )
+        other_picking = self.create_picking(
+            self.picking_type_pick,
+            products_info=self.pack_4apples_info,
+            confirm=True,
+            assign=True,
+        )
+
+        new_batch = Batch.create_batch(self.picking_type_pick.id, None)
+
+        # check outcome
+        self.assertIsNotNone(new_batch, "No batch created")
+        self.assertEqual(
+            len(new_batch.picking_ids),
+            1,
+            "Multiple pickings were included in the batch",
+        )
+        self.assertEqual(
+            new_batch.picking_ids[0].id,
+            other_picking.id,
+            "Does not include the expected picking",
+        )
+        self.assertEqual(batch.state, "done", "Old batch was not completed")
+
+    def test_create_batch_error_user_has_incomplete_batched_pickings(self):
+        """
+        Should error in case a the user already has a batch assigned
+        to him with incomplete pickings.
+
+        """
+        Batch = self.env["stock.picking.batch"]
+        Package = self.env["stock.quant.package"]
+        Batch = Batch.with_user(self.outbound_user)
+
+        # set a batch with a complete picking
+        self.create_quant(
+            self.apple.id,
+            self.test_stock_location_01.id,
+            4,
+            package_id=self.package_one.id,
+        )
+        self.create_picking(
+            self.picking_type_pick,
+            products_info=self.pack_4apples_info,
+            confirm=True,
+            assign=True,
+        )
+        batch = Batch.create_batch(self.picking_type_pick.id, None)
+
+        # create a new picking to be included in the new batch
+        other_pack = Package.get_package("test_other_package", create=True)
+        self.create_quant(
+            self.apple.id, self.test_location_01.id, 4, package_id=other_pack.id
+        )
+        self.create_picking(
+            self.picking_type_pick,
+            products_info=self.pack_4apples_info,
+            confirm=True,
+            assign=True,
+        )
+
+        # check pre-conditions
+        self.assertEqual(len(batch.picking_ids), 1)
+        self.assertEqual(batch.state, "in_progress")
+        self.assertEqual(batch.picking_ids[0].state, "assigned")
+
+        # method under test
+        with self.assertRaises(ValidationError) as err:
+            Batch.create_batch(self.picking_type_pick.id, None)
+
+        self.assertTrue(err.exception.name.startswith("The user already has pickings"))
+
+    def test_automatic_batch_done(self):
+        """Verifies the batch is done if the picking is complete"""
+        Batch = self.env["stock.picking.batch"]
+        Batch = Batch.with_user(self.outbound_user)
+
+        self.create_quant(
+            self.apple.id,
+            self.test_stock_location_01.id,
+            4,
+            package_id=self.package_one.id,
+        )
+        picking = self.create_picking(
+            self.picking_type_pick,
+            products_info=self.pack_4apples_info,
+            confirm=True,
+            assign=True,
+            location_dest_id=self.test_output_location_01.id,
+        )
+        batch = Batch.create_batch(self.picking_type_pick.id, None)
+        self.complete_picking(picking, validate=True)
+
+        # check pre-conditions
+        self.assertEqual(len(batch.picking_ids), 1)
+        self.assertEqual(batch.state, "done")
+        self.assertEqual(batch.picking_ids[0].state, "done")
+
+    def _create_valid_batch(self):
+        Batch = self.env["stock.picking.batch"]
+        Batch = Batch.with_user(self.outbound_user)
+
+        self.create_quant(
+            self.apple.id, self.test_location_01.id, 4, package_id=self.package_one.id
+        )
+        picking = self.create_picking(
+            self.picking_type_pick,
+            products_info=self.pack_4apples_info,
+            confirm=True,
+            assign=True,
+        )
+
+        return picking, Batch.create_batch(self.picking_type_pick.id, None)
