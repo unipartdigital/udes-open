d696c8fd65dad62ce810b485b6379860ffb08bcd
Armand Cela <armand.cela@unipart.io>
2022-04-21 11:46:05 +0100

    [ADD] udes_stock_picking_batch: Mobile UI: Batch picking functionalities
    Squashing in a single commit , easier to rebase with merged changes
    Story: 1174
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

---
+++ addons/udes_stock_picking_batch/models/res_users.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class ResUser(models.Model):
+    _inherit = "res.users"
+
+    u_location_category_ids = fields.Many2many(
+        comodel_name="stock.location.category", index=True, string="Location Category"
+    )
+
+    def get_user_location_categories(self):
+        """Get the location categories of the user"""
+        user = self.search([("id", "=", self.env.uid)])
+        if not user:
+            raise ValidationError(_("Cannot find user to get location categories."))
+
+        return user.u_location_category_ids
