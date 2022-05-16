c3b753b0440f4e25ccc8414f328dc7db803c439b
Jack Birch <jack.birch@unipart.io>
2022-05-16 15:58:52 +0100

    [IMP] udes_stock: Ensure that the prefixes of picking types are consistent with the UDES style and not Odoo's style.
    The sequence_code field is not needed in UDES as it is used strictly in the Odoo style sequence (which we do not want). When updating picking_types, if there was sequence_code field in the values to update, Odoo was hardcoded to update the sequences on the picking types to the Odoo prefix style. This resulted in changes to prefixes when changing data on the picking type, such as updating the picking_type with data that set a sequence_code.
    
    Additionally when the warehouse is created or updated with a new name or code, Odoo would update the picking type sequence prefixes.  _get_sequence_values() was were these new picking_type sequences prefixes were generated. This method has been overwritten to prevent this from happening.
    
    Some unit tests were added to ensure that writing to a picking type or updating the warehouse name didn't result in a change to the picking type sequence prefix.
    
    Story/1805
    
    Signed off by: Jack Birch <jack.birch@unipart.io>

--- addons/udes_stock/tests/test_stock_warehouse.py
+++ addons/udes_stock/tests/test_stock_warehouse.py
@@ -7,22 +7,37 @@ class TestStockWarehouseModel(BaseUDES):
         super(TestStockWarehouseModel, cls).setUpClass()
         cls.Warehouse = cls.env["stock.warehouse"]
 
-    def test01_get_test_picking_types(self):
+    def test_can_find_expected_picking_type(self):
         """Test checks if the test picking types are present"""
         test_picking_types = self.warehouse.get_picking_types().filtered(
             lambda pt: "TEST" in pt.name
         )
         self.assertEqual(len(test_picking_types), 6)
 
-    def test02_get_picking_types(self):
+    def test_finds_expected_number_of_picking_types(self):
         """Test checks if the test picking types and the normal pick types are present"""
         test_picking_types = self.warehouse.get_picking_types()
         self.assertEqual(len(test_picking_types), 9)
 
-    def test03_get_default_picking_types_new_warehouse(self):
+    def test_get_default_picking_types_new_warehouse(self):
         """Create a new warehouse and look at the default picking types"""
         # Create a new company creates a new warehouse
         test_company = self.create_company("test_company")
         new_warehouse = self.Warehouse.search([("company_id", "=", test_company.id)])
         test_picking_types = new_warehouse.get_picking_types()
         self.assertEqual(len(test_picking_types), 3)
+    
+    def test_check_picking_types_prefix_after_updating_warehouse_code(self):
+        """
+        Test that making changes to the warehouse does not cause the pickingtype prefixes to be reset. 
+        """ 
+        Pickingtype = self.env["stock.picking.type"]
+        
+        goods_in_prefix = Pickingtype.search([("name", "=", "Goods In")]).sequence_id.prefix
+        self.assertEqual(goods_in_prefix, "IN")
+        
+        self.warehouse.write({"code":"NEW_WAREHOUSE_CODE"})
+
+        goods_in_prefix = Pickingtype.search([("name", "=", "Goods In")]).sequence_id.prefix
+        self.assertEqual(goods_in_prefix, "IN")
+
