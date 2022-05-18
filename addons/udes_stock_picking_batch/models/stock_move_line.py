4b471578d5df9b302c9b23ba792a0e7d76634a50
Miquel PS <miquel.palahi.sitges@unipart.io>
2022-05-18 11:11:12 +0100

    [IMP] udes_stock_picking_batch: Increase max level task quants
    User-story: 1177
    
    Signed-off-by: Miquel PS <miquel.palahi.sitges@unipart.io>

--- addons/udes_stock_picking_batch/models/stock_move_line.py
+++ addons/udes_stock_picking_batch/models/stock_move_line.py
@@ -61,7 +61,7 @@ class StockMoveLine(models.Model):
         if user_scans == "product":
             task["type"] = "product"
             task["pick_quantity"] = sum(self.mapped("product_qty"))
-            task["quant_ids"] = self.get_quants().get_info()
+            task["quant_ids"] = self.get_quants().get_info(level=2)
         else:
             package = self.package_id
             package.ensure_one()
