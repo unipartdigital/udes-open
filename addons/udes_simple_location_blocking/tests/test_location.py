96048c60483478cc75f3d7838ff322db957c3d4d
Pete Pratt <peter.pratt@unipart.io>
2022-06-15 20:04:17 +0100

    [REF] udes_simple_location_blocking: Format unit test
    Run black, remove test numbering, replace deprecated exception.name with
    exception.args[0] instead.
    
    User-story: 2106
    
    Signed-off-by: Pete Pratt <peter.pratt@unipart.io>

--- addons/udes_simple_location_blocking/tests/test_location.py
+++ addons/udes_simple_location_blocking/tests/test_location.py
@@ -3,7 +3,7 @@ from .common import BaseBlocked
 
 
 class TestStockLocation(BaseBlocked):
-    def test01_check_blocked_wrong_prefix_type(self):
+    def test_check_blocked_wrong_prefix_type(self):
         """Call check blocked with wrong prefix type will
         raise a ValidationError.
 
@@ -12,9 +12,9 @@ class TestStockLocation(BaseBlocked):
         with self.assertRaises(ValidationError) as e:
             self.test_location_01.check_blocked(prefix=7)
 
-        self.assertEqual(e.exception.name, "Prefix parameter for check_blocked should be string")
+        self.assertEqual(e.exception.args[0], "Prefix parameter for check_blocked should be string")
 
-    def test02_check_blocked_one_location_one_blocked_location(self):
+    def test_check_blocked_one_location_one_blocked_location(self):
         """Block test_location_01 and check if it is blocked, which will
         raise a ValidationError.
         """
@@ -25,12 +25,13 @@ class TestStockLocation(BaseBlocked):
             self.test_location_01.check_blocked()
 
         self.assertEqual(
-            e.exception.name,
+            e.exception.args[0],
             " Location %s is blocked (reason: %s)."
-            " Please speak to a team leader to resolve the issue." % (self.test_location_01.name, self.test_location_01.u_blocked_reason)
+            " Please speak to a team leader to resolve the issue."
+            % (self.test_location_01.name, self.test_location_01.u_blocked_reason),
         )
 
-    def test03_check_blocked_two_locations_one_blocked_location(self):
+    def test_check_blocked_two_locations_one_blocked_location(self):
         """Block test_location_01 and check if either test_location_01
         or test_location_02 are blocked, which will raise a
         ValidationError.
@@ -42,40 +43,40 @@ class TestStockLocation(BaseBlocked):
             self.test_locations.check_blocked()
 
         self.assertEqual(
-            e.exception.name,
+            e.exception.args[0],
             " Location %s is blocked (reason: %s)."
-            " Please speak to a team leader to resolve the issue." % (self.test_location_01.name, self.test_location_01.u_blocked_reason)
+            " Please speak to a team leader to resolve the issue."
+            % (self.test_location_01.name, self.test_location_01.u_blocked_reason),
         )
 
-    def test04_prepare_blocked_msg_blocked_location_no_reason(self):
+    def test_prepare_blocked_msg_blocked_location_no_reason(self):
         """Prepare the message of a blocked location without reason."""
         self.test_location_01.u_blocked_reason = "Stock Damaged"
         self.test_location_01.u_blocked = True
         msg = self.test_location_01._prepare_blocked_msg()
         self.assertEqual(
             msg,
-            "Location %s is blocked (reason: %s)." % (self.test_location_01.name, self.test_location_01.u_blocked_reason)
+            "Location %s is blocked (reason: %s)."
+            % (self.test_location_01.name, self.test_location_01.u_blocked_reason),
         )
 
-    def test05_prepare_blocked_msg_blocked_location_with_reason(self):
+    def test_prepare_blocked_msg_blocked_location_with_reason(self):
         """Prepare the message of a blocked location with reason."""
         self.test_location_01.u_blocked_reason = "Stock Damaged"
         self.test_location_01.u_blocked = True
         msg = self.test_location_01._prepare_blocked_msg()
         self.assertEqual(
             msg,
-            "Location %s is blocked (reason: %s)." % (self.test_location_01.name, self.test_location_01.u_blocked_reason)
+            "Location %s is blocked (reason: %s)."
+            % (self.test_location_01.name, self.test_location_01.u_blocked_reason),
         )
 
-    def test06_prepare_blocked_msg_non_blocked_location(self):
+    def test_prepare_blocked_msg_non_blocked_location(self):
         """Prepare the message of a non blocked location."""
         msg = self.test_location_01._prepare_blocked_msg()
-        self.assertEqual(
-            msg,
-            "Location %s is not blocked." % self.test_location_01.name
-        )
+        self.assertEqual(msg, "Location %s is not blocked." % self.test_location_01.name)
 
-    def test07_onchange_blocked_to_false(self):
+    def test_onchange_blocked_to_false(self):
         """Check that blocked reason becomes empty when the
         location is unblocked
         """
@@ -84,7 +85,8 @@ class TestStockLocation(BaseBlocked):
         msg = self.test_location_01._prepare_blocked_msg()
         self.assertEqual(
             msg,
-            "Location %s is blocked (reason: %s)." % (self.test_location_01.name, self.test_location_01.u_blocked_reason)
+            "Location %s is blocked (reason: %s)."
+            % (self.test_location_01.name, self.test_location_01.u_blocked_reason),
         )
 
         self.test_location_01.u_blocked = False
@@ -92,7 +94,7 @@ class TestStockLocation(BaseBlocked):
         self.test_location_01.onchange_u_blocked()
         self.assertEqual(self.test_location_01.u_blocked_reason, "")
 
-    def test08_check_reserved_quants(self):
+    def test_check_reserved_quants(self):
         """Blocking a location with reserved quants will raise an error."""
         self.create_quant(self.apple.id, self.test_location_01.id, 10, reserved_quantity=10)
 
@@ -101,16 +103,16 @@ class TestStockLocation(BaseBlocked):
             self.test_location_01.u_blocked = True
 
         self.assertEqual(
-            e.exception.name, "Location cannot be blocked because it contains reserved stock."
+            e.exception.args[0], "Location cannot be blocked because it contains reserved stock."
         )
 
-    def test09_check_blocked_reason(self):
+    def test_check_blocked_reason(self):
         """Blocking a location with no reason given will raise and error"""
 
         with self.assertRaises(ValidationError) as e:
             self.test_location_01.u_blocked = True
 
         self.assertEqual(
-            e.exception.name,
+            e.exception.args[0],
             "A reason for blocking the locations is required when attempting to block a location.",
         )
