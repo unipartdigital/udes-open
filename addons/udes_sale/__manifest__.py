e2f4af9bd3ef9dfb6db0e68203b34148ccdcbff0
Peter Alabaster <peter.alabaster@unipart.io>
2022-06-23 13:18:12 +0100

    [ADD] udes_sale module for sale/account specific modifications
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.io>
    
    User-story: 1997

---
+++ addons/udes_sale/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "UDES Sale Functionality",
+    "version": "14.0",
+    "summary": "Sale, and Account extensions for UDES",
+    "description": "Extension of sale and account models for UDES",
+    "depends": [
+        "sale",
+        "account",
+    ],
+    "category": "Sales",
+    "sequence": 50,
+    "demo": [],
+    "data": [
+        "views/sale.xml",
+        "views/account.xml",
+    ],
+    "qweb": [],
+    "test": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
