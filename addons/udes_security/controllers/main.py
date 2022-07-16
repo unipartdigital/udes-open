00e2d8013831f5e167a4ba27a8bffda2eb6d5ea2
Peter Alabaster <peter.alabaster@unipart.io>
2022-07-16 17:45:42 +0100

    [ADD] udes_security: Check for action/menu/parent groups on load
    Firstly determine the menuitem which is for the action_id specified.
      This is achieved by doing a fuzzy SQL search, due to action
      being a Reference field, it's stored in the db with the relational
      model name as a prefix to the id. The IDs from different action
      models can not collide
    Then recursively check up the chain of parent menuitems whether the
      groups on the menuitem and action are defined, and if so - whether
      the user has any of these groups.
      If the user does not have access to any in the chain,
      then they will not be permitted access to this action.
    There are scenarios where an action can be defined in the system with
      no menuitem associated, in which case, we also block access.
    
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.io>
    
    User-story: 2417

--- addons/udes_security/controllers/main.py
+++ addons/udes_security/controllers/main.py
@@ -5,10 +5,175 @@ from datetime import datetime
 
 from odoo import http, SUPERUSER_ID, _
 from odoo.http import request
+from odoo.exceptions import AccessError, ValidationError
 
-from odoo.addons.web.controllers.main import Binary, serialize_exception
+from odoo.addons.web.controllers.main import Binary, serialize_exception, Action
 
 _logger = logging.getLogger(__name__)
+
+
+class SecureAction(Action):
+    @http.route()
+    def load(self, action_id, additional_context=None):
+        """
+        Extend load to check menu level access rights before returning the super call,
+        to prevent users without sufficient permission accessing pages via direct URL
+        """
+        group_no_one_id = request.env.ref("base.group_no_one").id
+        group_trusted_user_id = request.env.ref("udes_security.group_trusted_user").id
+
+        menu_path, action_group_ids = self._get_menu_info_from_action_id(action_id)
+        user_group_ids = self._get_object_group_ids(
+            request.session.uid, "res_groups_users_rel", "uid"
+        )
+        # Check if user has access to the menu, trigger AccessError and log a warning if not
+        if not self._user_has_access_right(
+            action_group_ids, menu_path, user_group_ids, group_no_one_id, group_trusted_user_id
+        ):
+            _logger.warning(
+                f"User id: {request.session.uid} had access blocked to action id: {action_id}"
+            )
+            raise AccessError(_("You do not have permission to access this action"))
+        return super().load(action_id, additional_context=additional_context)
+
+    def _get_object_group_ids(self, obj_id, relation_table, inverse_name):
+        """
+        Bypass the ORM to get groups associated with the object id
+
+        :param: obj_id: int() res.users or ir.ui.menu id
+        :param: relation_name: str() of rel table for object and groups
+        :param: inverse_name: str() of inverse relation for rel table to object
+
+        :return: list() res.group ids
+        """
+        query = f"SELECT ARRAY_AGG(gid) FROM {relation_table} WHERE {relation_table}.{inverse_name} IN (%s)"
+        request.env.cr.execute(query, (obj_id,))
+        result = request.env.cr.fetchone()
+        return result and result[0] or []
+
+    def _get_menu_info_from_action_id(self, action_id):
+        """
+        Get the ir.ui.menu parent_path and actions group_ids from a given action_id
+        `IrUiMenu.sudo().search([("action.id", "=", action_id)])`
+        does not work as `action` is a Reference field. The other option would be to
+        fetch all ir.ui.menu records and then `.filtered()`, but this is a little slow,
+        so instead do a direct query to get only the relevant parts.
+
+        All models on the `action` Reference field on `ir.ui.menu` have the same
+        `_sequence` parameter, so there is no risk of colliding ids from separate models.
+
+        They are stored in the db like "ir.actions.act_window,90" or "ir.actions.server,242"
+        due to being a Reference field, so perform a fuzzy search on %,<id>% to get it
+
+        Be extra safe and ensure `action_id` is an int, as if the user enters text in the URL
+        for action= param, it will come through to here as a string
+
+        :param: action_id: int() for an action (ir.actions.server, ir.actions.act_window, etc..)
+
+        :return: parent_path_ids: list() of ids for all ir.ui.menu records in the hierarchy
+                 action_group_ids:   list() of ids for res.groups for the action
+        """
+        if not isinstance(action_id, int):
+            try:  # If you middle click a menu, action_id is a string..
+                action_id = int(action_id)
+            except ValueError:
+                raise ValidationError("Invalid action ID specified.")
+
+        parent_path_ids = []
+        action_group_ids = []
+
+        query = """
+            SELECT
+                menu.parent_path AS parent_path,
+                field.relation_table AS relation_table,
+                field.column1 AS inverse,
+                SPLIT_PART(menu.action, ',', '2') AS action_id
+            FROM
+                ir_ui_menu menu
+            LEFT JOIN ir_model model ON model.model = SPLIT_PART(menu.action, ',', '1')
+            LEFT JOIN ir_model_fields field ON field.model_id = model.id AND field.name = 'groups_id'
+            WHERE action LIKE '%%,%s'
+        """
+        request.env.cr.execute(query, (action_id,))
+        result = request.env.cr.fetchone()
+        if result:
+            (parent_path, relation_table, inverse, action_id) = result
+            # Get ids in order from parent --> child of the menus
+            parent_path_ids = [int(str_menu_id) for str_menu_id in parent_path.split("/")[:-1]]
+            if relation_table and inverse:  # Some ir.actions* models don't have groups_id column
+                action_group_ids = self._get_object_group_ids(action_id, relation_table, inverse)
+        else:
+            # Certain actions don't have a menu, but we still need to determine the model
+            # which the action_id is for to get its groups. This approach is slower.
+            # This will happen rarely, so not concerned about optimising.
+            models = [
+                "ir.actions.act_window",
+                "ir.actions.server",
+                "ir.actions.client",
+                "ir.actions.report",
+                "ir.actions.act_url",
+            ]
+            for model in models:
+                action = request.env[model].sudo().search([("id", "=", action_id)], order="id")
+                if action and hasattr(action, "groups_id"):
+                    action_group_ids = action.groups_id.ids
+                    # Let _user_has_access_right() run at least once but give it
+                    # a menu id which will resolve to no result in the query
+                    parent_path_ids = [0]
+                    break
+        return parent_path_ids, action_group_ids
+
+    def _user_has_access_right(
+        self, action_group_ids, parent_path, user_group_ids, group_no_one_id, group_trusted_user_id
+    ):
+        """
+        Check user access rights against action, menu, and menu parent.
+
+        Start at the end of the parent_path, and pop the last item,
+        before recursively calling with the new parent_path until either:
+        1.) The user has no groups which are specified against the action or menu (return False)
+        2.) The user has groups specified against the action or menu, and no parent exists (return True)
+        3.) There are no groups specified against the action or menu, and no parent exists (return True)
+
+        This ensures that the user has access at least every menuitem in the chain of menuitems
+        leading to the menu/action they are trying to access, following the same logic of the web UI.
+
+        :param: action_group_ids: int() ids for res.groups
+        :param: parent_path: list() of ids referring to ir.ui.menu hierarchy
+        :param: user_group_ids: list() of res.group ids for the user
+        :param: group_no_one_id: int() id for Extra Rights/Technical Features group
+        :param: group_trusted_user_id: int() id for Other Extra Rights/Trusted User group
+
+        :return: bool() whether or not the user should be granted access
+        """
+        # Can have actions defined in the system with no menu associated. These should be blocked.
+        if not parent_path:
+            return False
+        menu_id = parent_path.pop()
+        group_ids_for_access = action_group_ids + self._get_object_group_ids(
+            menu_id, "ir_ui_menu_group_rel", "menu_id"
+        )
+        user_has_any_groups = any(g_id in user_group_ids for g_id in group_ids_for_access)
+        # If Technical Features is a group we need for access, then it implies Trusted User
+        # is required (ANDed) to see the menu by enabling debug mode
+        # (as all menus with Technical Features are hidden from normal UI)
+        if group_no_one_id in group_ids_for_access:
+            if group_trusted_user_id not in user_group_ids:
+                user_has_any_groups = False
+        if group_ids_for_access and not user_has_any_groups:
+            return False
+        else:
+            return (
+                self._user_has_access_right(
+                    action_group_ids,
+                    parent_path,
+                    user_group_ids,
+                    group_no_one_id,
+                    group_trusted_user_id,
+                )
+                if parent_path
+                else True
+            )
 
 
 class BinaryExtension(Binary):
