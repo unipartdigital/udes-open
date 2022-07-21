03b1e2b137df6db774de6b740f64afea3e2a67f6
Peter Alabaster <peter.alabaster@unipart.io>
2022-07-21 16:48:40 +0100

    [ADD] udes_security: Unit test for URL access
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.io>
    
    User-story: 2417

--- addons/udes_security/tests/__init__.py
+++ addons/udes_security/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_allowed_file_type
+from . import test_desktop_access
 from . import test_file_controllers
 from . import test_password_manager
 from . import test_res_archiving_restriction
