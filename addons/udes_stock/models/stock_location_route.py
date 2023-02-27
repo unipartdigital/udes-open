2192d2ba0a0ca6741dbbdebf8abd7ba0a849819d
armandcela <78373531+armandcela@users.noreply.github.com>
2023-02-27 17:02:38 +0000

    [ADD] udes_stock: Add visibility of archived rules for procurement routes (#763)
    Story: 3830
    
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- addons/udes_stock/models/stock_location_route.py
+++ addons/udes_stock/models/stock_location_route.py
@@ -6,3 +6,13 @@ class Route(models.Model):
 
     # Disable translation instead of renaming.
     name = fields.Char(translate=False)
+
+    def write(self, values):
+        """
+        When in a route active field is changed, set the context with active_test = False which will
+        ensure that will search even the inactive records and activate/inactivate them where needed.
+        """
+
+        if "active" in values and "active_test" not in self._context:
+            self = self.with_context(active_test=False)
+        return super(Route, self).write(values)
