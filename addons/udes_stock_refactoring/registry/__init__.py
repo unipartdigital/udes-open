f0f03e4cdec5e72ec0ccd5c4bda1c8634de891f3
armandcela <78373531+armandcela@users.noreply.github.com>
2023-05-18 17:00:46 +0100

    [ADD] udes_stock,udes_stock_refactoring: Migrating refactoring by quantity (#783)
    * [ADD] udes_stock,udes_stock_refactoring: Migrating refactoring by max quantity
    
    Story: 4150
    Signed-off-by: Armand Cela <armand.cela@unipart.io>
    
    * [ADD] udes_stock_refactoring: Porting udes11 unit tests
    
    Story: 4150
    Signed-off-by: Armand Cela <armand.cela@unipart.io>
    
    ---------
    
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- addons/udes_stock_refactoring/registry/__init__.py
+++ addons/udes_stock_refactoring/registry/__init__.py
@@ -3,5 +3,6 @@ from . import (
     refactor_batch_pickings_by_date,
     refactor_batch_pickings_by_date_priority,
     refactor_group_by_move_key,
-    refactor_group_by_move_line_key
+    refactor_group_by_move_line_key,
+    refactor_by_maximum_quantity,
 )
