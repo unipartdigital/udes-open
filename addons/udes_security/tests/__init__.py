af40dd1c767953edee1fb92195dd588e0eba7b3d
jayshree <jayshree.mahajan@unipart.com>
2023-08-18 19:01:41 +0530

    [IMP] udes_security: Strip EXIF data from image after upload
    story:4777
    
    Signed-off-by: jayshree <jayshree.mahajan@unipart.com>

--- addons/udes_security/tests/__init__.py
+++ addons/udes_security/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_desktop_access
 from . import test_trusted_user
 from . import test_no_redirect
 from . import test_password_complexity  # noqa: F401
+from . import test_stripped_exif_data
