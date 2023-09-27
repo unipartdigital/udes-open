af41f0b94498b4580ff1e51a0a9db6376e34a8a7
Peter Alabaster <peter.alabaster@unipart.com>
2023-09-27 14:59:42 +0100

    [META] udes_stock_cron: Refactor reserve_stock_function
    SET-159
    
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.com>

--- addons/udes_stock_cron/models/stock_picking.py
+++ addons/udes_stock_cron/models/stock_picking.py
@@ -1,20 +1,14 @@
-import time
-from psycopg2 import OperationalError, errorcodes
 from collections import defaultdict
 
 from odoo import models, _
 from odoo.exceptions import UserError
+from odoo.addons.udes_common.tools import odoo_retry
 
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
-PG_CONCURRENCY_ERRORS_TO_RETRY = (
-    errorcodes.LOCK_NOT_AVAILABLE,
-    errorcodes.SERIALIZATION_FAILURE,
-    errorcodes.DEADLOCK_DETECTED,
-)
 MAX_TRIES_ON_CONCURRENCY_FAILURE = 5
 
 
@@ -87,6 +81,156 @@ class StockPicking(models.Model):
 
         return unreservable_moves
 
+    def reserve_stock(self, batch_size=100):
+        """
+        Reserve stock according to the number of reservable pickings.
+
+        If this method is called on an empty recordset it will attempt to
+        reserve stock for all eligible picking types.  If the recordset is not
+        empty, it will reserve stock for the picks in the recordset, and it
+        will be the callers responsibility to make sure that the pickings belong
+        to at most one batch, otherwise this method cannot respect the priority
+        order of pickings, in this case the behaviour of this method is undefined.
+
+        Params:
+        batch_size: (int) The number of pickings to retrieve in each fetch from
+                          the database if the number of reservable pickings is
+                          unlimited.
+        """
+        PickingType = self.env["stock.picking.type"]
+        Quant = self.env["stock.quant"]
+
+        # Ensure all quants are merged and empty ones are deleted before starting to reserve stock.
+        # sudoing is handled by _quant_tasks.
+        Quant._quant_tasks()
+        # Getting here from a wizard - will have recordsets. CRON jobs - will be emptysets.
+        using_wizard = len(self) > 0
+        picking_types = (
+            self.picking_type_id
+            if using_wizard
+            else PickingType.search([("active", "=", True), ("u_num_reservable_pickings", "!=", 0)])
+        )
+
+        for picking_type in picking_types:
+            _logger.info("Reserving stock for picking type %r.", picking_type)
+            self.reserve_stock_for_picking_type(picking_type, using_wizard, batch_size)
+            _logger.info("Reserving stock for picking type %r completed.", picking_type)
+        return
+
+    def reserve_stock_for_picking_type(self, picking_type, using_wizard, batch_size):
+        """
+        Reserve stock for a particular picking type.
+
+        The picking type flags for reserving complete batches and handling
+        partial batches will be respected.
+
+        The number of reservable pickings is defined on the picking type.
+        0 reservable pickings means this function should not reserve stock
+        -1 reservable picking means all reservable stock should be reserved.
+
+        We will either reserve up to the reservation limit or until all
+        available picks have been reserved, depending on the value of
+        u_num_reservable_pickings.
+        However we must also take into account the atomic batch reservation
+        flag (u_reserve_batches) and the handle partial flag (u_handle_partials).
+        """
+
+        def generate_pickings():
+            """
+            Abstract iteration over fetched pickings.
+            If self is not empty, we yield the pickings by picking type, removing processed ones.
+            The state of these pickings is determined by the caller. The caller is also responsible
+            for ensuring the pickings in self belong to at most one batch.
+
+            If self is empty, we yield one by one all pickings which are not processed,
+            as we can not assume they are from the same batch.
+            """
+            while True:
+                if using_wizard:
+                    yield self.filtered(lambda p: p.picking_type_id == picking_type) - processed
+                else:
+                    domain = [
+                        ("picking_type_id", "=", picking_type.id),
+                        ("state", "=", "confirmed"),
+                    ]
+                    if processed:
+                        domain.append(("id", "not in", processed.ids))
+                    pickings = Picking.search(domain, limit=batch_size)
+                    if not pickings:
+                        break
+                    yield from pickings
+
+        Picking = self.env["stock.picking"]
+        # Reserve batches 'atomically' i.e reserve pickings until all pickings in a batch have
+        # been assigned, even if we exceed the number of reservable pickings.
+        # If the u_handle_partials is False, we should not reserve stock
+        # if the batch can not be completely reserved.
+        to_reserve = picking_type.u_num_reservable_pickings
+        processed = Picking.browse()
+
+        pickings_to_reserve = generate_pickings()
+        # Reserve stock until there is none left to reserve (or -1 - reserve all)
+        while to_reserve != 0:
+            pickings = next(pickings_to_reserve, Picking.browse())
+            _logger.info(f"Reserving stock for pickings {pickings}.")
+            if not pickings:
+                break  # No pickings left to process. Exit the loop.
+
+            if pickings.batch_id:
+                if pickings.batch_id.state == "draft":
+                    # Continue the loop (skip batch and don't process its pickings).
+                    processed |= pickings.batch_id.picking_ids
+                    continue
+                if picking_type.u_reserve_batches:
+                    # Process the batches pickings
+                    pickings = pickings.batch_id.picking_ids
+
+            # Pre-pass to remove unprocessable pickings
+            unreservable_moves = pickings._find_unreservable_moves(return_fast=bool(using_wizard))
+            if unreservable_moves:
+                moves = pickings.move_lines
+                moves_todo = (
+                    moves.filtered(lambda m: m.state in ["confirmed", "draft"]) - unreservable_moves
+                )
+                if not picking_type.u_handle_partials:
+                    # Add to processed to skip reservation.
+                    processed |= pickings
+
+                    if using_wizard:
+                        self.raise_insufficiency_error(unreservable_moves)
+                    continue
+                elif not moves_todo:
+                    # Add to processed to skip reservation.
+                    processed |= pickings
+                    continue
+
+            # Actually reserve the stock, inside a retry loop to handle potential concurrency issues
+            # Note that it is this loop that's responsible for handling try count.
+            # -1 is UserError raised by odoo_retry - but only if using_wizard is True.
+            data = odoo_retry(
+                self,
+                pickings._process_pickings,
+                MAX_TRIES_ON_CONCURRENCY_FAILURE,
+                raise_usererrors=using_wizard,
+            )()
+            tries = data.get("tries")
+            processed |= data.get("newly_processed")
+            if tries == -1:
+                continue
+            if tries >= MAX_TRIES_ON_CONCURRENCY_FAILURE:
+                break
+
+            # Incrementally commit to release picks as soon as possible and
+            # allow serialisation error to propagate to respect priority order
+            self.env.cr.commit()
+
+            # Determine reserved pickings from the move lines which got processed
+            to_reserve -= len(data.get("mls").picking_id)
+
+            if using_wizard:
+                # Only perform one iteration if we had all of our pickings scoped in to begin with
+                break
+
     def _reserve_stock_assign(self):
         """Perform assign of the pickings at the move level because refactoring
         may change the pickings.
@@ -95,208 +239,35 @@ class StockPicking(models.Model):
         # Assign at the move level disabling refactoring
         moves = self.move_lines
         assigned_moves = moves._action_assign()
-        return assigned_moves.mapped("picking_id")
+        return assigned_moves.picking_id
 
-    def reserve_stock(self, batch_size=100):
+    def raise_insufficiency_error(self, moves):
+        """Helper function to raise error for insufficent stock."""
+        products = moves.product_id.name_get()
+        picks = moves.mapped("picking_id.name")
+        product_names = ", ".join(sorted({p[1] for p in products}))
+        raise UserError(
+            _("Unable to reserve stock for products %s for pickings %s.") % (product_names, picks)
+        )
+
+    def _process_pickings(self):
         """
-        Reserve stock according to the number of reservable pickings.
-        If this method is called on an empty recordset it will attempt to
-        reserve stock for all eligible picking types.  If the recordset is not
-        empty, it will reserve stock for the picks in the recordset. If the
-        recordset is not empty, it is the callers responsibility to make sure
-        that the pickings belong to at most one batch, otherwise this method
-        cannot respect the priority order of pickings, in this case the
-        behaviour of this method is undefined.
-        In either scenario the picking type flags for reserving complete
-        batches and handling partial batches are respected.
-        The number of reservable pickings is defined on the picking type.
-        0 reservable pickings means this function should not reserve stock
-        -1 reservable picking means all reservable stock should be reserved.
-
-        Params:
-        batch_size: (int) The number of pickings to retrieve in each fetch from
-                          the database if the number of reservable pickings is
-                          unlimited.
+        Helper function to actually process the pickings in `self`.
+        Called while being wrapped by odoo_retry, hence the return values being a dict.
         """
+        unsatisfied_state = lambda p: p.state not in ("assigned", "cancel", "done")
+        newly_processed = self._reserve_stock_assign()
+        self.batch_id._compute_state()
 
-        Picking = self.env["stock.picking"]
-        PickingType = self.env["stock.picking.type"]
-        Quant = self.env["stock.quant"]
+        unsatisfied = newly_processed.filtered(unsatisfied_state)
 
-        def raise_insufficiency_error(moves):
-            """Helper function to raise error for insufficent stock."""
-            products = moves.product_id.name_get()
-            picks = moves.mapped("picking_id.name")
-            product_names = ", ".join(sorted({p[1] for p in products}))
-            raise UserError(
-                _("Unable to reserve stock for products %s for pickings %s.")
-                % (product_names, picks)
-            )
-
-        # Making sure that quants are merged and deleted the empty ones before starting the reserve
-        # stock. No need to run with sudo as the methods inside _quant_tasks have already sudo
-        # where needed.
-        Quant._quant_tasks()
-
-        if self:
-            picking_types = self.picking_type_id
-        else:
-            picking_types = PickingType.search(
-                [("active", "=", True), ("u_num_reservable_pickings", "!=", 0)]
-            )
-
-        # We will either reserve up to the reservation limit or until all
-        # available picks have been reserved, depending on the value of
-        # u_num_reservable_pickings.
-        # However we must also take into account the atomic batch reservation
-        # flag (u_reserve_batches) and the handle partial flag
-        # (u_handle_partials).
-
-        for picking_type in picking_types:
-            _logger.info("Reserving stock for picking type %r.", picking_type)
-
-            # We want to reserve batches atomically, that is we will
-            # reserve pickings until all pickings in a batch have been
-            # assigned, even if we exceed the number of reservable pickings.
-            # However, the value of the handle partial flag is false we
-            # should not reserve stock if the batch cannot be completely
-            # reserved.
-            to_reserve = picking_type.u_num_reservable_pickings
-            reserve_all = to_reserve == -1
-            base_domain = [
-                ("picking_type_id", "=", picking_type.id),
-                ("state", "=", "confirmed"),
-            ]
-            limit = batch_size if reserve_all else to_reserve
-            processed = Picking.browse()
-
-            unsatisfied_state = lambda p: p.state not in ("assigned", "cancel", "done")
-
-            def generate_pickings():
-                """Abstract iteration over fetched pickings."""
-                while True:
-                    if self:
-                        # Removed processed pickings from self
-                        yield self.filtered(lambda p: p.picking_type_id == picking_type) - processed
-                    else:
-                        domain = base_domain[:]
-                        if processed:
-                            domain.append(("id", "not in", processed.ids))
-                        pickings = Picking.search(domain, limit=batch_size)
-                        if not pickings:
-                            break
-                        yield from pickings
-
-            pickings_to_reserve = generate_pickings()
-
-            while reserve_all or to_reserve > 0:
-                pickings = next(pickings_to_reserve, Picking.browse())
-                _logger.info(f"Reserving stock for pickings {pickings}.")
-                if not pickings:
-                    # No pickings left to process.
-                    # If u_num_reservable_pickings is -1, or there are
-                    # fewer available pickings that the limit, the loop must
-                    # terminate here.
-                    break
-
-                batch = pickings.batch_id
-                if batch and batch.state == "draft":
-                    # Add to seen pickings so that we don't try to process
-                    # this batch again.
-                    processed |= batch.picking_ids
-                    continue
-
-                if batch and picking_type.u_reserve_batches:
-                    pickings = batch.picking_ids
-
-                # Check for available stock and add pickings which we cannot
-                # fulfill to the processed recordset. The logic here
-                # should mirror that within the savepoint below, expect here we
-                # do not have the move lines.
-                unreservable_moves = pickings._find_unreservable_moves(return_fast=bool(self))
-                if unreservable_moves:
-                    moves = pickings.move_lines
-                    moves_todo = (
-                        moves.filtered(lambda m: m.state in ["confirmed", "draft"])
-                        - unreservable_moves
-                    )
-                    if not picking_type.u_handle_partials:
-                        # Add to processed to skip reservation.
-                        processed |= pickings
-
-                        if self:
-                            raise_insufficiency_error(unreservable_moves)
-                        continue
-                    elif not moves_todo:
-                        # Add to processed to skip reservation.
-                        processed |= pickings
-                        continue
-
-                # MPS: mimic Odoo's retry behaviour
-                tries = 0
-                while True:
-                    try:
-                        with self.env.cr.savepoint():
-                            pickings = pickings._reserve_stock_assign()
-                            batch._compute_state()
-
-                            processed |= pickings
-
-                            unsatisfied = pickings.filtered(unsatisfied_state)
-
-                            mls = pickings.move_line_ids
-                            if unsatisfied:
-                                # Unreserve if the picking type cannot handle partials or it
-                                # can but there is nothing allocated (no stock.move.lines)
-                                if not picking_type.u_handle_partials or not mls:
-                                    # construct error message, report only products
-                                    # that are unreservable.
-                                    moves = unsatisfied.move_lines.filtered(unsatisfied_state)
-                                    raise_insufficiency_error(moves)
-                            break
-                    except UserError as e:
-                        self.invalidate_cache()
-                        # Only propagate the error if the function has been
-                        # manually triggered
-                        if self:
-                            raise e
-                        tries = -1
-                        break
-                    except OperationalError as e:
-                        self.invalidate_cache()
-                        if e.pgcode not in PG_CONCURRENCY_ERRORS_TO_RETRY:
-                            raise
-                        if tries >= MAX_TRIES_ON_CONCURRENCY_FAILURE:
-                            _logger.info(
-                                "%s, maximum number of tries reached" % errorcodes.lookup(e.pgcode)
-                            )
-                            break
-                        tries += 1
-                        wait_time = 1
-                        _logger.info(
-                            "%s, retry %d/%d in %.04f sec..."
-                            % (
-                                errorcodes.lookup(e.pgcode),
-                                tries,
-                                MAX_TRIES_ON_CONCURRENCY_FAILURE,
-                                wait_time,
-                            )
-                        )
-                        time.sleep(wait_time)
-                if tries == -1:
-                    continue
-                if tries >= MAX_TRIES_ON_CONCURRENCY_FAILURE:
-                    break
-
-                # Incrementally commit to release picks as soon as possible and
-                # allow serialisation error to propagate to respect priority
-                # order
-                self.env.cr.commit()
-                # Only count as reserved the number of pickings at mls
-                to_reserve -= len(mls.picking_id)
-
-                if self:
-                    # Only process the specified pickings
-                    break
-            _logger.info("Reserving stock for picking type %r completed.", picking_type)
-        return
+        mls = newly_processed.move_line_ids
+        if unsatisfied:
+            # Unreserve if the picking type cannot handle partials or it
+            # can but there is nothing allocated (no stock.move.lines)
+            if not self.picking_type_id.u_handle_partials or not mls:
+                # construct error message, report only products
+                # that are unreservable.
+                moves = unsatisfied.move_lines.filtered(unsatisfied_state)
+                self.raise_insufficiency_error(moves)
+        return {"newly_processed": newly_processed, "mls": mls}
