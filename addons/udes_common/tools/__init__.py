06f92dc6e272c7d95290106c88e0863e583a3df8
Peter Alabaster <peter.alabaster@unipart.com>
2023-09-29 12:14:35 +0100

    [ADD] udes_common: odoo_retry tool function
    SET-159
    
    Signed-off-by: Peter Alabaster <peter.alabaster@unipart.com>

--- addons/udes_common/tools/__init__.py
+++ addons/udes_common/tools/__init__.py
@@ -4,3 +4,4 @@ from .iterators import batched, ranged, sliced
 from .statistics import Statistics
 from .tracing import QueryTracer
 from .relational_field_operators import RelationalFieldOperators as RelFieldOps
+from .retry import odoo_retry as odoo_retry
