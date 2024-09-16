40f7cfc2d28d60ea2e8969a14ca8183cd9c92df2
Adam Patrick <adam.patrick@unipart.com>
2024-09-16 15:11:01 +0100

    [FIX] udes_stock: Set u_picking_zone_id to a stored field
    Signed-off-by: Adam Patrick <adam.patrick@unipart.com>

--- addons/udes_stock/models/stock_location.py
+++ addons/udes_stock/models/stock_location.py
@@ -160,7 +160,7 @@ class StockLocation(models.Model):
         help="This location's picking zone, if it is in a picking zone.",
         compute="_compute_picking_zone_id",
         readonly=True,
-        store=False,
+        store=True,
     )
 
     def set_u_heatmap_data_updated(self, vals):
@@ -266,6 +266,7 @@ class StockLocation(models.Model):
 
             location.u_countable_state = state
 
+    @api.depends("u_picking_zone_id", "location_id", "u_is_picking_zone")
     def _compute_picking_zone_id(self):
         """Set the location that is the picking zone for the location in self.
 
