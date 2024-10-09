fbdb77bd5081a66c7bb346d6d09186590dfe704c
Kevin Dwyer <kevin.dwyer@unipart.com>
2024-10-09 17:04:58 +0100

    [FIX] udes_common: Change return type of PreciseDatetime.now
    SE-1471
    
    - PreciseDatetime.now returns a datetime object instead of a string
    - Add tests for PreciseDatetime
    - Alias PreciseDatetime.from_string to to_datetime
    
    Odoo have made this alias in core, but it means that in our subclass,
    from_string points to the core to_datetime, not the method in our class.
    
    Make the same alias to fix it.
    
    Signed-off-by: Nicole Carrington <nicole.carrington@unipart.com>

---
+++ addons/udes_common/tests/test_fields.py
@@ -0,0 +1,31 @@
+"""Unit tests for PreciseDatetime field."""
+
+import datetime as dt
+
+from odoo import fields, models
+from odoo.fields import Datetime
+from odoo.tests.common import SavepointCase, tagged
+
+from ..models.fields import PreciseDatetime
+
+
+@tagged("post_install", "-at_install")
+class PreciseDatetimeTestCase(SavepointCase):
+    """Tests for the PreciseDatetime field type."""
+
+    def test_now_returns_datetime_instance(self):
+        """Our class should return type as the superclass."""
+        imprecise_now = Datetime.now()
+        precise_now = PreciseDatetime.now()
+
+        self.assertTrue(isinstance(imprecise_now, dt.datetime))
+        self.assertTrue(isinstance(precise_now, dt.datetime))
+
+    def test_from_string_can_handle_microseconds(self):
+        """The from string method should handle microseconds."""
+        date_string = '2024-10-10 09:15:05.123'
+        datetime_value = Datetime.from_string(date_string[:4])
+        precise_datetime_value = PreciseDatetime.from_string(date_string)
+
+        self.assertTrue(isinstance(datetime_value, dt.datetime))
+        self.assertTrue(isinstance(precise_datetime_value, dt.datetime))
