2bfc87ef7eb983c425b24eb404fba6408d2e3e05
Darpan Bhatt <darpan.bhatt@unipart.com>
2025-11-17 22:52:30 +0530

    [ADD] udes_stock: Enable index on product_barcode fields
    SE-2217
    
    Signed-off-by: Darpan Bhatt <darpan.bhatt@unipart.com>

--- addons/udes_stock/models/product_barcode.py
+++ addons/udes_stock/models/product_barcode.py
@@ -11,10 +11,10 @@ class ProductBarcode(models.Model):
     _order = "name"
 
     name = fields.Char(
-        "Barcode", copy=False, required=True,
+        "Barcode", copy=False, required=True, index=True,
         help="International Article Number used for product identification."
     )
-    product_tmpl_id = fields.Many2one("product.template", string="Product", required=True, ondelete="cascade")
+    product_tmpl_id = fields.Many2one("product.template", string="Product", required=True, ondelete="cascade", index=True)
 
     _sql_constraints = [
         ("name_uniq", "unique(name)", "A barcode can only be assigned to one product!"),
