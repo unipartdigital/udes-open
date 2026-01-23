2eb7c37afd27d24d978fb3fd1b3d114cb8c1275e
Armand Cela <armand.cela@unipart.io>
2026-01-23 15:39:33 +0000

    [ADD] udes_sale_stock: Fix getting sale order from package
    SE-2394
    Signed-off-by: Armand Cela <armand.cela@unipart.io>

--- addons/udes_sale_stock/models/stock_quant_package.py
+++ addons/udes_sale_stock/models/stock_quant_package.py
@@ -4,12 +4,18 @@ from odoo import models
 class StockQuantPackage(models.Model):
     _inherit = "stock.quant.package"
 
-    def get_sale_order(self):
+    def get_sale_order(self, states=None):
         """
         Getting sale order from a package.
+
+        When is a unique package, example parcel packages will check states assigned or done,
+        otherwise only assigned move lines.
         """
         self.ensure_one()
-        mls = self.get_move_lines_of_children([("state", "in", ["assigned", "done"])])
+        move_line_states = ["assigned"]
+        if states:
+            move_line_states += states
+        mls = self.get_move_lines_of_children([("state", "in", move_line_states)])
 
         sale = mls.move_id.sale_line_id.order_id
         return sale
